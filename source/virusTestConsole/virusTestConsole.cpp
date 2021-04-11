da09b589cd2467edb8ea647d97768cd6e15fc0ab
Lyve <lyve2909+github@gmail.com>
2021-04-11 22:52:30 +0200

    some hacks to get virus running

--- source/virusTestConsole/virusTestConsole.cpp
+++ source/virusTestConsole/virusTestConsole.cpp
@@ -9,21 +9,22 @@ int main(int _argc, char* _argv[])
 {
 	UnitTests tests;
 
-	constexpr TWord g_memorySize = 0x400000;	// 128k words beginning at 0x200000
+	constexpr TWord g_memorySize = 0x040000;	// 128k words beginning at 0x200000
 
 	const DefaultMemoryMap memoryMap;
 	Memory memory(memoryMap, g_memorySize);
 
-	memory.setExternalMemory(0x20000, true);	// @steven was the starting address 0x200000?
+	memory.setExternalMemory(0x020000, true);
 
 	Peripherals56362 periph;
 
 	DSP dsp(memory, &periph, &periph);
 
-
 	virusLib::RomLoader romLoader;
 	if(!romLoader.loadFromFile(dsp, _argv[1]))
 		return -1;
+
+//	memory.saveAssembly("Virus_P.asm", 0, g_memorySize, false, true);
 
 	dsp.setPeriph(0, &periph);
 
@@ -31,25 +32,52 @@ int main(int _argc, char* _argv[])
 
 	std::thread dspThread([&]()
 	{
-//		dsp.setPC(0x000da6);
+		auto counter = -0x2BFD9;
+		auto timer = false;
 
 		while(true)
-			dsp.exec();
+		{
+			if(dsp.getPC() == 0x2BFF4)
+				timer = true;
+			if(dsp.getPC() == 0x02c013)
+				timer = false;
+
+			if(timer)
+			{
+				if(counter++ == 100)
+				{
+					dsp.injectInterrupt(Vba_TIMER0_Overflow);
+					counter = 0;
+				}
+			}
+			dsp.exec();			
+		}
 	});
 
 	constexpr size_t sampleCount = 4;
+	constexpr size_t channelsIn = 2;
+	constexpr size_t channelsOut = 6;
 
 	float inputDataL[sampleCount] = {1,-1,0.5f,-0.5f};
 	float inputDataR[sampleCount] = {1,-1,0.5f,-0.5f};
 	float outputDataL[sampleCount] = {0,0,0,0};
 	float outputDataR[sampleCount] = {0,0,0,0};
 
-	float* audioIn[2] = {inputDataL, inputDataR};
-	float* audioOut[6] = {outputDataL, outputDataR, outputDataL, outputDataR, outputDataL, outputDataR};
+	float* audioIn [channelsIn ] = {inputDataL, inputDataR};
+	float* audioOut[channelsOut] = {outputDataL, outputDataR, outputDataL, outputDataR, outputDataL, outputDataR};
 
 	while(true)
 	{
+		LOG("Deliver Audio");
 		periph.getEsai().processAudioInterleaved(audioIn, audioOut, sampleCount, 2, 6);
+
+		for(auto c=0; c<channelsOut; ++c)
+		{
+			for(auto i=0; i<sampleCount; ++i)
+			{
+				assert(audioOut[c][i] == 0.0f && "We've to sample data!");
+			}
+		}
 	}
 
 	return 0;
