623c2f1ad25a56d5b98a42de8614cc1a0778f4a0
Lyve <lyve2909+github@gmail.com>
2021-04-25 17:08:33 +0200

    fix midi in never cleared

--- source/virusLib/plugin.cpp
+++ source/virusLib/plugin.cpp
@@ -24,6 +24,7 @@ namespace virusLib
 	void Plugin::process(float** _inputs, float** _outputs, size_t _count)
 	{
 		m_device->process(_inputs, _outputs, _count, m_midiIn, m_midiOut);
+		m_midiIn.clear();
 		m_midiOut.clear();	// TODO
 	}
 }
