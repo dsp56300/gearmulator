f8b150a1cef0005cbcadfa52a624ef698b4e2279
Lyve <lyve2909+github@gmail.com>
2021-07-04 22:12:10 +0200

    fix audio input not sent to DSP

--- source/virusLib/audiobuffer.h
+++ source/virusLib/audiobuffer.h
@@ -26,6 +26,7 @@ namespace virusLib
 		}
 
 		void insertZeroes(size_t _size);
+		const std::vector<float>& getChannel(const size_t _channel) { return m_data[_channel]; }
 
 		AudioBuffer(size_t _channelCount = 2, size_t _capacity = 1024);
 	private:
