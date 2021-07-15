1a61e788080d39eafea351d24bb53ceb40cf015b
dsp56300 <dsp56300@users.noreply.github.com>
2021-07-15 17:15:40 +0200

    fix wrong formatting of bank & preset numbers in log

--- source/virusLib/microcontroller.cpp
+++ source/virusLib/microcontroller.cpp
@@ -342,7 +342,7 @@ bool Microcontroller::sendSysex(const std::vector<uint8_t>& _data, bool _cancelI
 			{
 				const uint8_t bank = _data[7];
 				const uint8_t program = _data[8];
-				LOG("Dump Single, Bank " << bank << ", program " << program);
+				LOG("Dump Single, Bank " << (int)bank << ", (int)program " << program);
 				TPreset dump;
 				std::copy_n(_data.data() + g_sysexPresetHeaderSize, dump.size(), dump.begin());
 				return writeSingle(bank, program, dump, _cancelIfFull);
@@ -351,7 +351,7 @@ bool Microcontroller::sendSysex(const std::vector<uint8_t>& _data, bool _cancelI
 			{
 				const uint8_t bank = _data[7];
 				const uint8_t program = _data[8];
-				LOG("Dump Multi, Bank " << bank << ", program " << program);
+				LOG("Dump Multi, Bank " << (int)bank << ", (int)program " << program);
 				TPreset dump;
 				std::copy_n(_data.data() + g_sysexPresetHeaderSize, dump.size(), dump.begin());
 				return writeMulti(bank, program, dump, _cancelIfFull);
@@ -360,7 +360,7 @@ bool Microcontroller::sendSysex(const std::vector<uint8_t>& _data, bool _cancelI
 			{
 				const uint8_t bank = _data[7];
 				const uint8_t program = _data[8];
-				LOG("Request Single, Bank " << bank << ", program " << program);
+				LOG("Request Single, Bank " << (int)bank << ", (int)program " << program);
 				buildSingleResponse(bank, program);
 				break;
 			}
@@ -368,7 +368,7 @@ bool Microcontroller::sendSysex(const std::vector<uint8_t>& _data, bool _cancelI
 			{
 				const uint8_t bank = _data[7];
 				const uint8_t program = _data[8];
-				LOG("Request Multi, Bank " << bank << ", program " << program);
+				LOG("Request Multi, Bank " << (int)bank << ", (int)program " << program);
 				buildMultiResponse(bank, program);
 				break;
 			}
