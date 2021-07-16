7e7592755d354921f38c2d6c1e13f07c646d9694
dsp56300 <dsp56300@users.noreply.github.com>
2021-07-16 03:24:50 +0200

    skip old way of zipping binaries, cmake does everything now

--- scripts/ci_win.bat
+++ scripts/ci_win.bat
@@ -1,7 +1,3 @@
 pushd ..\
 call build_win64.bat
 popd
-7z a ..\deploy\dsp56300emuVST2Win.zip ..\temp\cmake_win64\source\jucePlugin\jucePlugin_artefacts\Release\VST\DSP56300Emu.dll
-7z a ..\deploy\dsp56300emuVST3Win.zip ..\temp\cmake_win64\source\jucePlugin\jucePlugin_artefacts\Release\VST3\DSP56300Emu.vst3\Contents\x86_64-win\DSP56300Emu.vst3
-7z a ..\deploy\dsp56300emuTestConsoleWin.zip ..\temp\cmake_win64\Release\virusTestConsole.exe ..\deploy\win\*.bat
-IF %ERRORLEVEL% NEQ 0 exit -3
