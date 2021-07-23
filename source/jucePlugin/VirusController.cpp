58b1a1098403ea7451da8368e676ee979853ee2e
Tal Aviram <me@talaviram.com>
2021-07-23 18:42:52 +0300

    controller - add sysex msg constructor

--- source/jucePlugin/VirusController.cpp
+++ source/jucePlugin/VirusController.cpp
@@ -7,6 +7,14 @@ namespace Virus
     {
     }
 
+    std::vector<uint8_t> Controller::constructMessage(SysEx msg)
+    {
+        uint8_t start[] = {0xf0, 0x00, 0x20, 0x33, 0x01, static_cast<uint8_t>(m_deviceId)};
+        msg.insert(msg.begin(), std::begin(start), std::end(start));
+        msg.push_back(0xf7);
+        return msg;
+    }
+
     void Controller::dispatchVirusOut(const std::vector<synthLib::SMidiEvent> &newData)
     {
         m_virusOut = newData;
