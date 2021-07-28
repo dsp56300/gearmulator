4baa9bc8fd221808d52d93115342847d9e4948ff
dsp56300 <dsp56300@users.noreply.github.com>
2021-07-28 17:16:26 +0200

    helper script to install dependencies

---
+++ linux_dependencies.sh
@@ -0,0 +1,2 @@
+sudo apt-get -y install gcc g++ cmake rpm
+sudo apt-get -y install libfreetype6-dev libx11-dev libxinerama-dev libxrandr-dev libxcursor-dev mesa-common-dev libasound2-dev freeglut3-dev libxcomposite-dev
