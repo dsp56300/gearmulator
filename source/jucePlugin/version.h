0bc5cb8f1b5449440fbcf968ecec3f5744f65c9c
dsp56300 <dsp56300@users.noreply.github.com>
2021-08-11 21:08:19 +0200

    bump version to 1.1.9

--- source/jucePlugin/version.h
+++ source/jucePlugin/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-static constexpr const char* const g_pluginVersionString = "1.18";
-static constexpr uint32_t g_pluginVersion = 118;
+static constexpr const char* const g_pluginVersionString = "1.19";
+static constexpr uint32_t g_pluginVersion = 119;
