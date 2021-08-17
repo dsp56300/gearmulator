e4a93acb9c6c1158492bec03e543bad975731a43
Tal Aviram <me@talaviram.com>
2021-08-17 19:48:51 +0300

    editor - add temporary part selector.

--- source/jucePlugin/PluginEditor.cpp
+++ source/jucePlugin/PluginEditor.cpp
@@ -12,7 +12,7 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(AudioPluginAudi
 
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+	setSize(800, 400);
 
 	m_btSingleMode.setRadioGroupId(0x3cf);
 	m_btMultiMode.setRadioGroupId(0x3cf);
@@ -28,6 +28,26 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(AudioPluginAudi
 	m_btMultiMode.setClickingTogglesState(true);
 	m_btMultiMode.setTopLeftPosition(m_btSingleMode.getPosition().x + m_btSingleMode.getWidth() + 10, 0);
 	m_btMultiMode.setSize(120,30);
+
+	for (auto pt = 0; pt < 16; pt++)
+	{
+		m_partSelectors[pt].onClick = [this, pt]() {
+			auto bankA = processorRef.getController().getSinglePresetNames(0);
+			auto bankB = processorRef.getController().getSinglePresetNames(1);
+			juce::PopupMenu pA;
+			juce::PopupMenu pB;
+			for (auto i = 0; i < 128; i++)
+			{
+				pA.addItem(bankA[i], [this, i, pt] { processorRef.getController().setCurrentPartPreset(pt, 0, i); });
+				pB.addItem(bankB[i], [this, i, pt] { processorRef.getController().setCurrentPartPreset(pt, 1, i); });
+			}
+			juce::PopupMenu selector;
+			selector.addSubMenu("Bank A", pA);
+			selector.addSubMenu("Bank B", pB);
+			selector.showMenu(juce::PopupMenu::Options());
+		};
+		addAndMakeVisible(m_partSelectors[pt]);
+	}
 
 	addAndMakeVisible(m_tempEditor);
 }
@@ -50,13 +70,21 @@ void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 	if(!processorRef.isPluginValid())
 		message += "\n\nNo ROM, no sound!\nCopy ROM next to plugin, must end with .bin";
 
-	g.drawFittedText(message, getLocalBounds(), juce::Justification::centred, 2);
-    g.drawFittedText("To donate: paypal.me/dsp56300", getLocalBounds(), juce::Justification::centredBottom, 2);
+	g.drawFittedText(message, getLocalBounds().removeFromLeft(400).removeFromBottom(45), juce::Justification::centred,
+					 2);
+	g.drawFittedText("To donate: paypal.me/dsp56300", getLocalBounds().removeFromRight(400).removeFromTop(35),
+					 juce::Justification::centred, 2);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
 {
 	// This is generally where you'll want to lay out the positions of any
 	// subcomponents in your editor..
-	m_tempEditor.setBounds(0, 35, getWidth(), getHeight() - 35);
+	auto area = getLocalBounds();
+	area.removeFromTop(35);
+	m_tempEditor.setBounds(area.removeFromRight(400));
+	for (auto pt = 0; pt < 16; pt++)
+	{
+		m_partSelectors[pt].setBounds(area.removeFromTop(20));
+	}
 }
