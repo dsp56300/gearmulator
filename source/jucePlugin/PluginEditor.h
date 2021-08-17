93bd6c16a48e8d434ee15dc21e4803480c8e3101
Tal Aviram <me@talaviram.com>
2021-08-17 19:52:34 +0300

    editor - ugly way for updating preset name.

--- source/jucePlugin/PluginEditor.h
+++ source/jucePlugin/PluginEditor.h
@@ -3,7 +3,7 @@
 #include "PluginProcessor.h"
 
 //==============================================================================
-class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor, private juce::Timer
 {
 public:
     explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
@@ -14,9 +14,10 @@ public:
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    AudioPluginAudioProcessor& processorRef;
+	void timerCallback() override;
+	// This reference is provided as a quick way for your editor to
+	// access the processor object that created it.
+	AudioPluginAudioProcessor& processorRef;
 
 	juce::GenericAudioProcessorEditor m_tempEditor;
 
