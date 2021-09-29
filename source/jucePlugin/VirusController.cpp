190e5119d37d96ce3ee61515817eb71a16a57d56
lol-catz <lolcatz21@protonmail.com>
2021-09-29 18:04:31 -0500

    fix "Unison Mode" labeling/values

--- source/jucePlugin/VirusController.cpp
+++ source/jucePlugin/VirusController.cpp
@@ -936,10 +936,30 @@ namespace Virus
 		default:  return juce::String(v);
 		}
 	}
-	
+
 	juce::String numToUnisonMode(float v, Parameter::Description)
 	{
-		return juce::roundToInt(v) == 0 ? "Off" : "Twin " + juce::String(juce::roundToInt(v));
+		const auto ridx = juce::roundToInt(v);
+		switch (ridx)
+		{
+		case 0:  return "Off";
+		case 1:  return "Twin";
+		case 2:  return "3";
+		case 3:  return "4";
+		case 4:  return "5";
+		case 5:  return "6";
+		case 6:  return "7";
+		case 7:  return "8";
+		case 8:  return "9";
+		case 9:  return "10";
+		case 10: return "11";
+		case 11: return "12";
+		case 12: return "13";
+		case 13: return "14";
+		case 14: return "15";
+		case 15: return "16";
+		default: return juce::String(v);
+		}
 	}
 
 	juce::String numToOscFmMode(float idx, Parameter::Description)
