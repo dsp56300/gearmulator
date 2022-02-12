07015b5cfcbd9e173ac8303392c92dc43358874a
Steven <synthemu+steven@gmail.com>
2022-02-12 13:07:32 +0100

    Add support for loading TI installation files

---
+++ source/virusLib/unpacker.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <thread>
+#include <utility>
+#include <vector>
+
+#include "dsp56kEmu/types.h"
+#include "utils.h"
+
+namespace virusLib
+{
+
+	class ROMUnpacker
+	{
+	public:
+		enum AccessVirusTIModel
+		{
+			VirusTI = 1,
+			VirusTISnow = 2,
+			VirusTI2 = 3,
+		};
+
+		struct Firmware
+		{
+			std::vector<char> DSP;
+			std::vector<std::vector<char>> Presets;
+
+			[[nodiscard]] bool isValid() const
+			{
+				return !DSP.empty();
+			}
+		};
+
+		static bool isValidInstaller(std::istream& _file);
+
+		static Firmware getFirmware(std::istream& _file, AccessVirusTIModel _model);
+
+	private:
+		struct Chunk
+		{
+			std::string name;
+			uint32_t size;
+			std::vector<char> data;
+		};
+
+		static std::string getVtiFilename(AccessVirusTIModel _model);
+
+		static std::vector<Chunk> getChunks(std::istream& _file);
+
+		static std::vector<char> unpackFile(std::vector<Chunk>& _chunks, char _fileId);
+	};
+
+}
