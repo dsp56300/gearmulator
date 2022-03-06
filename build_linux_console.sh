e01ae88875625c5f76e2ec4bcb296312a79a9c7b
dsp56300 <dsp56300@users.noreply.github.com>
2022-03-06 16:38:09 +0100

    make sure that console build creates a release build + enable parallel builds

--- build_linux_console.sh
+++ build_linux_console.sh
@@ -1,3 +1,3 @@
-cmake . -B ./temp/cmake_linux_console -Dgearmulator_BUILD_JUCEPLUGIN=OFF
+cmake . -B ./temp/cmake_linux_console -Dgearmulator_BUILD_JUCEPLUGIN=OFF -DCMAKE_BUILD_TYPE=Release
 cd ./temp/cmake_linux_console
-cmake --build . --config Release
+cmake --build . --config Release -j 4
