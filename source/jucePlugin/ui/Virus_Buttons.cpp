8a5a04367f4614d76998108b02ca8bfc929edf72
dsp56300 <dsp56300@users.noreply.github.com>
2022-03-10 23:21:00 +0100

    fix toggle switches showing the opposite state

--- source/jucePlugin/ui/Virus_Buttons.cpp
+++ source/jucePlugin/ui/Virus_Buttons.cpp
@@ -7,12 +7,12 @@ namespace Buttons
 {
     HandleButton::HandleButton() : DrawableButton("HandleButton", DrawableButton::ImageRaw)
     {
-	    const auto up = Drawable::createFromImageData(BinaryData::Handle_18x47_png, BinaryData::Handle_18x47_pngSize);
-        const auto down = up->createCopy();
+	    const auto down = Drawable::createFromImageData(BinaryData::Handle_18x47_png, BinaryData::Handle_18x47_pngSize);
+        const auto up = down->createCopy();
         setColour(DrawableButton::ColourIds::backgroundColourId, Colours::transparentBlack);
         setColour(DrawableButton::ColourIds::backgroundOnColourId, Colours::transparentBlack);
         setClickingTogglesState(true);
-        down->setOriginWithOriginalSize({-18, 0});
+		up->setOriginWithOriginalSize({-18, 0});
         setImages(down.get(), nullptr, up.get(), nullptr, up.get(), nullptr, down.get());
     }
 
