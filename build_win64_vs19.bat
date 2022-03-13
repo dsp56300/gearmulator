d944a1b85c3c1869cd1091db721c154eb309bb85
dsp56300 <dsp56300@users.noreply.github.com>
2022-03-13 17:17:22 +0100

    build script for Visual Studio 2019

---
+++ build_win64_vs19.bat
@@ -0,0 +1,15 @@
+set outdir=temp\cmake_win64\
+cmake . -B %outdir% -G "Visual Studio 16 2019" -A x64
+IF %ERRORLEVEL% NEQ 0 (
+	popd 
+	exit /B 2
+)
+pushd %outdir%
+cmake --build . --config Release
+IF %ERRORLEVEL% NEQ 0 (
+	popd 
+	exit /B 2
+)
+cpack -G ZIP
+popd
+move /y %outdir%*.zip deploy\
