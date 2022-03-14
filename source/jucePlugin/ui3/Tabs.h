73ec876f25bc4a9833ea8f9c49449a218bad3f84
dsp56300 <dsp56300@users.noreply.github.com>
2022-03-14 23:32:31 +0100

    implement tabs

--- source/jucePlugin/ui3/Tabs.h
+++ source/jucePlugin/ui3/Tabs.h
@@ -1,12 +1,27 @@
 #pragma once
 
+#include <vector>
+
+namespace juce
+{
+	class Button;
+	class Component;
+}
+
 namespace genericVirusUI
 {
+	class VirusEditor;
+
 	class Tabs
 	{
 	public:
-		Tabs(VirusEditor& _editor) : m_editor(_editor) {}
+		explicit Tabs(VirusEditor& _editor);
 	private:
+		void setPage(size_t _page);
+
 		VirusEditor& m_editor;
+
+		std::vector<juce::Component*> m_tabs;
+		std::vector<juce::Button*> m_tabButtons;
 	};
 }
