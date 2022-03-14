02628e10623400d11a5edf59d96a357a72967461
dsp56300 <dsp56300@users.noreply.github.com>
2022-03-14 20:42:17 +0100

    add support for text buttons & labels

--- source/jucePlugin/genericUI/uiObjectStyle.h
+++ source/jucePlugin/genericUI/uiObjectStyle.h
@@ -10,6 +10,8 @@ namespace genericUI
 	class UiObjectStyle : public juce::LookAndFeel_V4
 	{
 	public:
+		UiObjectStyle();
+
 		void setTileSize(int _w, int _h);
 		void setDrawable(juce::Drawable* _drawable);
 		void setTextHeight(int _height);
@@ -17,11 +19,19 @@ namespace genericUI
 		virtual void apply(Editor& _editor, const UiObject& _object);
 
 	protected:
+		juce::Font getComboBoxFont(juce::ComboBox&) override;
+		juce::Font getLabelFont(juce::Label&) override;
+		juce::Font getPopupMenuFont() override;
+		juce::Font getTextButtonFont(juce::TextButton&, int buttonHeight) override;
+
 		void applyFontProperties(juce::Font& _font) const;
 
 		int m_tileSizeX = 0;
 		int m_tileSizeY = 0;
 		juce::Drawable* m_drawable = nullptr;
 		int m_textHeight = 0;
+		std::string m_text;
+		juce::Colour m_color = juce::Colour(0xffffffff);
+		juce::Justification m_align = 0;
 	};
 }
