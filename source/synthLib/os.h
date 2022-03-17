426f20a328c39f582bc74048356f3494ff264dc0
dsp56300 <dsp56300@users.noreply.github.com>
2022-03-17 22:37:34 +0100

    add more directory helper functions

--- source/synthLib/os.h
+++ source/synthLib/os.h
@@ -1,10 +1,18 @@
 #pragma once
 #include <string>
+#include <vector>
 
 namespace synthLib
 {
     std::string getModulePath();
+
+	std::string getCurrentDirectory();
+	bool createDirectory(const std::string& _dir);;
+
+	bool getDirectoryEntries(std::vector<std::string>& _files, const std::string& _folder);
+
     std::string findROM(size_t _minSize, size_t _maxSize);
 	std::string findROM(size_t _expectedSize = 524288);
+
 	void setFlushDenormalsToZero();
 } // namespace synthLib
