176b68561102550fe55bb7e8bd8b3b5389ac9daf
dsp56300 <dsp56300@users.noreply.github.com>
2022-03-19 02:52:39 +0100

    add font

--- source/jucePlugin/skins/Trancy/assets.cmake
+++ source/jucePlugin/skins/Trancy/assets.cmake
@@ -9,6 +9,7 @@ set(ASSETS_VirusC_Trancy
 	${CMAKE_CURRENT_LIST_DIR}/btn_main_4.png
 	${CMAKE_CURRENT_LIST_DIR}/btn_main_5.png
 	${CMAKE_CURRENT_LIST_DIR}/btn_save_preset.png
+	${CMAKE_CURRENT_LIST_DIR}/Digital.ttf
 	${CMAKE_CURRENT_LIST_DIR}/knob_1_128.png
 	${CMAKE_CURRENT_LIST_DIR}/knob_2_128.png
 	${CMAKE_CURRENT_LIST_DIR}/main_background.png
