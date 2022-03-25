a66ee5494f3b0b84045d69e43a4c3e5616e2c02e
dsp56300 <dsp56300@users.noreply.github.com>
2022-03-26 00:45:23 +0100

    rework integration test to keep/write a compare file

--- source/virusIntegrationTest/integrationTest.h
+++ source/virusIntegrationTest/integrationTest.h
@@ -14,14 +14,20 @@ public:
 	int run();
 
 private:
-	bool loadReferenceFile(const std::string& _filename);
+	struct File
+	{
+		std::vector<uint8_t> file;
+		synthLib::Data data;
+	};
+
+	bool loadAudioFile(File& _dst, const std::string& _filename) const;
 	int runCompare();
 	int runCreate(int _lengthSeconds);
+	int createAudioFile(File& _dst, const std::string& _prefix, uint32_t _sampleCount);
 
 	const CommandLine& m_cmd;
 	const std::string m_romFile;
 	ConsoleApp m_app;
 
-	std::vector<uint8_t> m_referenceFileData;
-	synthLib::Data m_referenceData;
+	File m_referenceFile;
 };
