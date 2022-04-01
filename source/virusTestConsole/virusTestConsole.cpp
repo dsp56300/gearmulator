1f34ce05aba16cceb51ccc74a01d3b20a079407a
dsp56300 <dsp56300@users.noreply.github.com>
2022-04-01 17:45:14 +0200

    fix stack overflow error

--- source/virusTestConsole/virusTestConsole.cpp
+++ source/virusTestConsole/virusTestConsole.cpp
@@ -38,9 +38,10 @@ int main(int _argc, char* _argv[])
 		return -1;
 	}
 
-	ConsoleApp app(romFile, 0x100000, 0x020000);
+	std::unique_ptr<ConsoleApp> app;
+	app.reset(new ConsoleApp(romFile, 0x100000, 0x020000));
 
-	if(!app.isValid())
+	if(!app->isValid())
 	{
 		std::cout << "ROM file " << romFile << " couldn't be loaded. Make sure tha the ROM file is valid" << std::endl;
 		ConsoleApp::waitReturn();
@@ -52,14 +53,14 @@ int main(int _argc, char* _argv[])
 		const std::string name = _argv[1];
 		if(hasExtension(name, ".mid"))
 		{
-			if(!app.loadDemo(name))
+			if(!app->loadDemo(name))
 			{
 				std::cout << "Failed to load demo from '" << name << "', make sure that the file contains a demo" << std::endl;
 				ConsoleApp::waitReturn();
 				return -1;
 			}
 		}
-		else if(!app.loadSingle(name))
+		else if(!app->loadSingle(name))
 		{
 			std::cout << "Failed to find preset '" << _argv[1] << "', make sure to use a ROM that contains it" << std::endl;
 			ConsoleApp::waitReturn();
@@ -68,13 +69,13 @@ int main(int _argc, char* _argv[])
 	}
 	else
 	{
-//		app.loadSingle(3, 56);		// Impact  MS
-		app.loadSingle(0, 51);		// IndiArp BC
+//		app->loadSingle(3, 56);		// Impact  MS
+		app->loadSingle(0, 0);		// IndiArp BC
 	}
 
-	const std::string audioFilename = app.getSingleNameAsFilename();
+	const std::string audioFilename = app->getSingleNameAsFilename();
 
-	app.run(audioFilename);
+	app->run(audioFilename);
 
 	return 0;
 }
