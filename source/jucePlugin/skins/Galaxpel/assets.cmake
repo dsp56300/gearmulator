90ae52e11f31b71eb00bfa9aa0d92b00557d6d5b
dsp56300 <dsp56300@users.noreply.github.com>
2022-04-02 02:39:41 +0200

    add assets for Galaxpel skin (1st version)

---
+++ source/jucePlugin/skins/Galaxpel/assets.cmake
@@ -0,0 +1,31 @@
+set(ASSETS_VirusC_Galaxpel
+	${CMAKE_CURRENT_LIST_DIR}/arp_page_midiPorts.png
+	${CMAKE_CURRENT_LIST_DIR}/arp-btn-306x83.png
+	${CMAKE_CURRENT_LIST_DIR}/arp-onoff-72x37.png
+	${CMAKE_CURRENT_LIST_DIR}/arp-select-104x72.png
+	${CMAKE_CURRENT_LIST_DIR}/BEBASNEUE_BOLD-_1_.ttf
+	${CMAKE_CURRENT_LIST_DIR}/BEBASNEUE_REGULAR-_1_.ttf
+	${CMAKE_CURRENT_LIST_DIR}/btn2-52x36.png
+	${CMAKE_CURRENT_LIST_DIR}/chain-btn2-62x36.png
+	${CMAKE_CURRENT_LIST_DIR}/chain-btn-36x62.png
+	${CMAKE_CURRENT_LIST_DIR}/efcts-btn-306x83.png
+	${CMAKE_CURRENT_LIST_DIR}/env-pol-radio-btns-38x66.png
+	${CMAKE_CURRENT_LIST_DIR}/fx_page_reverb.png
+	${CMAKE_CURRENT_LIST_DIR}/fx_page_reverbOverlay.png
+	${CMAKE_CURRENT_LIST_DIR}/knob_2-104x104.png
+	${CMAKE_CURRENT_LIST_DIR}/lfo_page.png
+	${CMAKE_CURRENT_LIST_DIR}/lfo-btn-306x83.png
+	${CMAKE_CURRENT_LIST_DIR}/load-push-btn-112x121.png
+	${CMAKE_CURRENT_LIST_DIR}/main_bg.png
+	${CMAKE_CURRENT_LIST_DIR}/Mult_36x36_x2i.png
+	${CMAKE_CURRENT_LIST_DIR}/osc_page-sub-fixed.png
+	${CMAKE_CURRENT_LIST_DIR}/osc-btn-306x83.png
+	${CMAKE_CURRENT_LIST_DIR}/presest_btn_select_86x60_x2.png
+	${CMAKE_CURRENT_LIST_DIR}/preset-selected-light-74x68.png
+	${CMAKE_CURRENT_LIST_DIR}/presets-push-btn-118x121.png
+	${CMAKE_CURRENT_LIST_DIR}/save-push-btn-80x121.png
+	${CMAKE_CURRENT_LIST_DIR}/sync-38x38.png
+	${CMAKE_CURRENT_LIST_DIR}/toggle-btn-39x102.png
+
+	${CMAKE_CURRENT_LIST_DIR}/VirusC_Galaxpel.json
+)
