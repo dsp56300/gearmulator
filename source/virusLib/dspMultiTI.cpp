7e4cc1284e9e36548454f0a70d0805bafe48bcfb
dsp56300 <dsp56300@users.noreply.github.com>
2022-04-24 02:14:03 +0200

    fix USB outputs on Master were silent

--- source/virusLib/dspMultiTI.cpp
+++ source/virusLib/dspMultiTI.cpp
@@ -17,10 +17,6 @@ namespace virusLib
 		const float* inputs[] = {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
 		float* outputs[] = {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
 
-		m_buffer.ensureSize(s*3);
-
-		float* outputsMix[] = {m_buffer.getChannel(0), m_buffer.getChannel(1), m_buffer.getChannel(2), m_buffer.getChannel(3), nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
-
 		inputs[0] = _inputs[0];
 		inputs[1] = _inputs[1];
 
@@ -33,34 +29,35 @@ namespace virusLib
 		m_dsp2.getPeriphX().getEsai().processAudioInputInterleaved(inputs, s, _latency);
 		m_dsp2.getPeriphY().getEsai().processAudioInputInterleaved(inputs, s * 3, _latency);
 
+		// ESAI outputs
+
 		// DAC outputs on the master are fed by ESAI in regular fashion
-		outputs[4] = _outputs[1];
-		outputs[5] = _outputs[0];
-		outputs[6] = _outputs[3];
-		outputs[7] = _outputs[2];
-		outputs[8] = _outputs[5];
-		outputs[9] = _outputs[4];
+		outputs[4] = _outputs[1];		outputs[5] = _outputs[0];
+		outputs[6] = _outputs[3];		outputs[7] = _outputs[2];
+		outputs[8] = _outputs[5];		outputs[9] = _outputs[4];
 
 		getPeriphX().getEsai().processAudioOutputInterleaved(outputs, s, _latency);
 
-		// USB outputs on master are send to the slave via ESAI_1 in 1/3 interleaved format => unpack it
+		// USB outputs on the Slave are sent in regular fashion via ESAI
+		outputs[4] = _outputs[7];		outputs[5] = _outputs[6];
+		outputs[6] = _outputs[9];		outputs[7] = _outputs[8];
+		outputs[8] = _outputs[11];		outputs[9] = _outputs[10];
+
+		m_dsp2.getPeriphX().getEsai().processAudioOutputInterleaved(outputs, s, _latency);
+
+		// ESAI_1 outputs
+		m_buffer.ensureSize(s*3);
+
+		float* outputsMix[] = {m_buffer.getChannel(0), m_buffer.getChannel(1), m_buffer.getChannel(2), m_buffer.getChannel(3), nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+
+		// USB outputs on the Master are sent to the Slave via ESAI_1 in 1/3 interleaved format => unpack it
 		getPeriphY().getEsai().processAudioOutputInterleaved(outputsMix, s * 3, _latency);
 
 		mixEsai1Output(_outputs[6] , _outputs[7] , outputsMix[3], outputsMix[2], s, 2, 1);	// USB 1
 		mixEsai1Output(_outputs[8] , _outputs[9] , outputsMix[0], outputsMix[1], s, 2, 0);	// USB 2
 		mixEsai1Output(_outputs[10], _outputs[11], outputsMix[2], outputsMix[3], s, 2, 0);	// USB 3
 
-		// USB outputs on the Slave are send in regular fashion via ESAI
-		outputs[4] = _outputs[7];
-		outputs[5] = _outputs[6];
-		outputs[6] = _outputs[9];
-		outputs[7] = _outputs[8];
-		outputs[8] = _outputs[11];
-		outputs[9] = _outputs[10];
-
-		m_dsp2.getPeriphX().getEsai().processAudioOutputInterleaved(outputs, s, _latency);
-
-		// DAC outputs are send via ESAI_1 to the Master in 1/3 interleaved format => unpack
+		// DAC outputs on the Slave are send via ESAI_1 to the Master in 1/3 interleaved format => unpack it
 		m_dsp2.getPeriphY().getEsai().processAudioOutputInterleaved(outputsMix, s*3, _latency);
 
 		mixEsai1Output(_outputs[0], _outputs[1], outputsMix[0], outputsMix[1], s, 1, 2);	// Out 1
