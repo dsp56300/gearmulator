27b4bcfc1d7b6f11244d4417f0991fb4f7d15bd4
dsp56300 <dsp56300@users.noreply.github.com>
2022-04-26 21:25:18 +0200

    fix left & right channels reversed

--- source/virusLib/dspSingleSnow.cpp
+++ source/virusLib/dspSingleSnow.cpp
@@ -10,14 +10,14 @@ namespace virusLib
 	{
 		const auto s = static_cast<uint32_t>(_samples);
 
-		const float* inputs0[] = {_inputs[0], _inputs[1], nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
-		const float* inputs1[] = {_inputs[2], _inputs[3], nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+		const float* inputs0[] = {_inputs[1], _inputs[0], nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+		const float* inputs1[] = {_inputs[3], _inputs[2], nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
 
 		getPeriphX().getEsai().processAudioInputInterleaved(inputs0, s, _latency);
 		getPeriphY().getEsai().processAudioInputInterleaved(inputs1, s, _latency);
 
-		float* outputs0[] = {_outputs[0], _outputs[1], nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
-		float* outputs1[] = {nullptr, nullptr, _outputs[2], _outputs[3], _outputs[4], _outputs[5], _outputs[6], _outputs[7], nullptr, nullptr, nullptr, nullptr};
+		float* outputs0[] = {_outputs[1], _outputs[0], nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+		float* outputs1[] = {nullptr, nullptr, _outputs[3], _outputs[2], _outputs[5], _outputs[4], _outputs[7], _outputs[6], nullptr, nullptr, nullptr, nullptr};
 
 		getPeriphX().getEsai().processAudioOutputInterleaved(outputs0, s, _latency);
 		getPeriphY().getEsai().processAudioOutputInterleaved(outputs1, s, _latency);
