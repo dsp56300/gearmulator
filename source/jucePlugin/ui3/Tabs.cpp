67b199643649d6e29d698ec5a0268c5896270feb
dsp56300 <dsp56300@users.noreply.github.com>
2022-05-14 01:01:30 +0200

    add support for generic tab groups

--- source/jucePlugin/ui3/Tabs.cpp
+++ source/jucePlugin/ui3/Tabs.cpp
@@ -4,35 +4,12 @@
 
 namespace genericVirusUI
 {
-	Tabs::Tabs(VirusEditor& _editor): m_editor(_editor)
+	Tabs::Tabs(const VirusEditor& _editor)
+		: genericUI::TabGroup("pages"
+			, {"page_osc", "page_lfo", "page_fx", "page_arp", "page_presets"}
+			, {"TabOsc", "TabLfo", "TabEffects", "TabArp", "Presets"}
+		)
 	{
-		m_tabs.push_back(m_editor.findComponent("page_osc"));
-		m_tabs.push_back(m_editor.findComponent("page_lfo"));
-		m_tabs.push_back(m_editor.findComponent("page_fx"));
-		m_tabs.push_back(m_editor.findComponent("page_arp"));
-		m_tabs.push_back(m_editor.findComponent("page_presets"));
-
-		m_tabButtons.push_back(m_editor.findComponentT<juce::Button>("TabOsc"));
-		m_tabButtons.push_back(m_editor.findComponentT<juce::Button>("TabLfo"));
-		m_tabButtons.push_back(m_editor.findComponentT<juce::Button>("TabEffects"));
-		m_tabButtons.push_back(m_editor.findComponentT<juce::Button>("TabArp"));
-		m_tabButtons.push_back(m_editor.findComponentT<juce::Button>("Presets"));
-
-		if(m_tabs.size() != m_tabButtons.size())
-			throw std::runtime_error("Number of tabs does not match number of tab buttons, not all requested objects have been found");
-
-		for(size_t i=0; i<m_tabButtons.size(); ++i)
-			m_tabButtons[i]->onClick = [this, i] { setPage(i); };
-
-		setPage(0);
-	}
-
-	void Tabs::setPage(const size_t _page) const
-	{
-		for(size_t i=0; i<m_tabs.size(); ++i)
-		{
-			m_tabs[i]->setVisible(_page == i);
-			m_tabButtons[i]->setToggleState(_page == i, juce::dontSendNotification);
-		}
+		create(_editor);
 	}
 }
