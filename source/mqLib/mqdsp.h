7fa0d58137b1e8c2ac5de11b8a6f30677b1135c9
dsp56300 <dsp56300@users.noreply.github.com>
2022-05-29 15:01:33 +0200

    boot microQ DSP

---
+++ source/mqLib/mqdsp.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "dsp56kEmu/dsp.h"
+#include "dsp56kEmu/peripherals.h"
+
+namespace mqLib
+{
+	class MqDsp
+	{
+	public:
+		MqDsp();
+		void exec();
+
+		dsp56k::HDI08& hdi08()
+		{
+			return m_periphX.getHDI08();
+		}
+		dsp56k::DSP& dsp()
+		{
+			return m_dsp;
+		}
+
+	private:
+		dsp56k::Peripherals56362 m_periphX;
+		dsp56k::PeripheralsNop m_periphNop;
+		dsp56k::Memory m_memory;
+		dsp56k::DSP m_dsp;
+	};
+}
