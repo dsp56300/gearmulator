2c099638b61cbd0f0a9dd36b195dde751c151259
dsp56300 <dsp56300@users.noreply.github.com>
2022-05-29 15:01:59 +0200

    connect uc to DSP

--- source/mqTestConsole/mqTestConsole.cpp
+++ source/mqTestConsole/mqTestConsole.cpp
@@ -1,9 +1,18 @@
 #include <memory>
 
+#include "../mqLib/mqdsp.h"
 #include "../synthLib/os.h"
 
 #include "../mqLib/rom.h"
 #include "../mqLib/mqmc.h"
+#include "dsp56kEmu/dspthread.h"
+
+namespace mqLib
+{
+	class MqDsp;
+}
+
+#define DSP 1
 
 int main(int _argc, char* _argv[])
 {
@@ -12,11 +21,43 @@ int main(int _argc, char* _argv[])
 	mqLib::ROM rom(romFile);
 
 	std::unique_ptr<mqLib::MqMc> mc;
-	mc.reset(new mqLib::MqMc(rom));
+	std::unique_ptr<mqLib::MqDsp> dsp;
 
+	mc.reset(new mqLib::MqMc(rom));
+	dsp.reset(new mqLib::MqDsp());
+
+	std::deque<uint32_t> txData;
+
+#if DSP
+	dsp56k::DSPThread dspThread(dsp->dsp());
+	bool dumpDSP = false;
+#endif
 	while(true)
 	{
 		mc->exec();
+
+#if DSP
+		mc->hdi08().pollTx(txData);
+		for (const uint32_t data : txData)
+			dsp->hdi08().writeRX(&data, 1);
+
+		uint8_t interruptAddr;
+		if(mc->hdi08().pollInterruptRequest(interruptAddr))
+			dsp->dsp().injectInterrupt(interruptAddr);
+
+		while(dsp->hdi08().hasTX())
+		{
+			mc->hdi08().writeRx(dsp->hdi08().readTX());
+		}
+
+		if(dumpDSP)
+		{
+			dsp->dsp().coreDump();
+			dumpDSP = false;
+		}
+#endif
+		txData.clear();
 	}
+
 	return 0;
 }
