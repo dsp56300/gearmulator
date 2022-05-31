45a4b7b31df653bc77b2b3907d49138dd7e25b0e
dsp56300 <dsp56300@users.noreply.github.com>
2022-05-31 20:39:46 +0200

    fix DSP external memory not initialized

--- source/mqLib/mqdsp.cpp
+++ source/mqLib/mqdsp.cpp
@@ -21,6 +21,8 @@ namespace mqLib
 			m_memory.set(dsp56k::MemArea_P, i + g_bootCodeBase, code);
 		}
 
+		m_memory.setExternalMemory(0x80000, true);
+
 		m_memory.saveAssembly("dspBootDisasm.asm", g_bootCodeBase, static_cast<uint32_t>(std::size(g_dspBootCode)), true, true, &m_periphX, nullptr);
 
 		m_dsp.setPC(g_bootCodeBase);
