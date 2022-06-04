7948fca3322d0def4b4a89cd4500e6d83e740a4f
dsp56300 <dsp56300@users.noreply.github.com>
2022-06-04 04:27:12 +0200

    write ESAI to WAV

--- source/mqLib/mqdsp.cpp
+++ source/mqLib/mqdsp.cpp
@@ -36,7 +36,7 @@ namespace mqLib
 		m_dsp.setPC(g_bootCodeBase);
 		m_dsp.regs().omr.var |= OMR_MA | OMR_MB | OMR_MC | OMR_MD;
 
-		m_periphX.getEsai().writeEmptyAudioIn(1024);
+		m_periphX.getEsai().writeEmptyAudioIn(2048);
 	}
 
 	void MqDsp::exec()
