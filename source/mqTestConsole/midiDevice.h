9361a2f99cc580d76475eba430a128096438bb08
dsp56300 <dsp56300@users.noreply.github.com>
2022-06-20 22:15:56 +0200

    fix midi ports couldn't be opened if the same device exists for in and out

--- source/mqTestConsole/midiDevice.h
+++ source/mqTestConsole/midiDevice.h
@@ -5,11 +5,14 @@
 class MidiDevice : public Device
 {
 public:
-	MidiDevice(const std::string& _deviceName);
+	MidiDevice(const std::string& _deviceName, bool _output);
 	~MidiDevice() override = default;
 
 	static std::string getDeviceNameFromId(int _devId);
 
 	std::string deviceNameFromId(int _devId) const override;
 	int deviceIdFromName(const std::string& _devName) const override;
+
+private:
+	const bool m_output;
 };
