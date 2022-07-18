22e18ff77daddc17671537ccf5e9a8de9f514fd8
dsp56300 <dsp56300@users.noreply.github.com>
2022-07-18 22:54:15 +0200

    extract async writer from audioprocessor into separate class

--- source/synthLib/wavWriter.cpp
+++ source/synthLib/wavWriter.cpp
@@ -4,6 +4,11 @@
 
 #include <map>
 #include <cassert>
+#include <chrono>
+#include <mutex>
+#include <thread>
+
+#include "dsp56kEmu/types.h"
 
 namespace synthLib
 {
@@ -95,5 +100,105 @@ namespace synthLib
 		fclose(handle);
 
 		return true;
+	}
+
+	void WavWriter::writeWord(std::vector<uint8_t>& _dst, dsp56k::TWord _word)
+	{
+		const auto d = reinterpret_cast<const uint8_t*>(&_word);
+		_dst.push_back(d[0]);
+		_dst.push_back(d[1]);
+		_dst.push_back(d[2]);
+	}
+
+	AsyncWriter::AsyncWriter(std::string _filename, uint32_t _samplerate, bool _measureSilence)
+	: m_filename(std::move(_filename))
+	, m_samplerate(_samplerate)
+	, m_measureSilence(_measureSilence)
+	{
+		m_thread.reset(new std::thread([&]()
+		{
+			threadWriteFunc();
+		}));
+	}
+
+	AsyncWriter::~AsyncWriter()
+	{
+		m_finished = true;
+
+		if(m_thread)
+		{
+			m_thread->join();
+			m_thread.reset();
+		}
+	}
+
+	void AsyncWriter::append(const std::function<void(std::vector<dsp56k::TWord>&)>& _func)
+	{
+		std::lock_guard lock(m_writeMutex);
+		_func(m_stereoOutput);
+	}
+
+	void AsyncWriter::threadWriteFunc()
+	{
+		synthLib::WavWriter writer;
+
+		std::vector<dsp56k::TWord> m_wordBuffer;
+		std::vector<uint8_t> m_byteBuffer;
+		m_byteBuffer.reserve(m_wordBuffer.capacity() * 3);
+
+		while(true)
+		{
+			{
+				std::lock_guard lock(m_writeMutex);
+				std::swap(m_wordBuffer, m_stereoOutput);
+			}
+
+			if(m_wordBuffer.empty() && m_byteBuffer.empty() && m_finished)
+				break;
+
+			if(!m_wordBuffer.empty())
+			{
+				for (const dsp56k::TWord w : m_wordBuffer)
+					WavWriter::writeWord(m_byteBuffer, w);
+
+				if(m_measureSilence)
+				{
+					bool isSilence = true;
+
+					for (const dsp56k::TWord w : m_wordBuffer)
+					{
+						constexpr dsp56k::TWord silenceThreshold = 0x1ff;
+						const bool silence = w < silenceThreshold || w >= (0xffffff - silenceThreshold);
+						if(!silence)
+						{
+							isSilence = false;
+							break;
+						}
+					}
+
+					if(isSilence)
+						m_silenceDuration += static_cast<uint32_t>(m_wordBuffer.size() >> 1);
+					else
+						m_silenceDuration = 0;
+				}
+
+				m_wordBuffer.clear();
+			}
+
+			if(!m_byteBuffer.empty())
+			{
+				if(writer.write(m_filename, 24, false, 2, static_cast<int>(m_samplerate), &m_byteBuffer[0], m_byteBuffer.size()))
+				{
+					m_byteBuffer.clear();
+				}
+				else if(m_finished)
+				{
+					LOG("Unable to write data to file " << m_filename << " but termination requested, file is missing data");
+					break;
+				}
+			}
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
 	}
 }
--- source/synthLib/wavWriter.h
+++ source/synthLib/wavWriter.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include <functional>
+#include <memory>
+#include <mutex>
+
 #include "wavTypes.h"
 
 #include <vector>
 #include <string>
+#include <thread>
+
+#include "dsp56kEmu/types.h"
 
 namespace synthLib
 {
@@ -17,7 +24,47 @@ namespace synthLib
 		{
 			return write(_filename, _bitsPerSample, isFloat, _channelCount, _samplerate, &_data[0], sizeof(T) * _data.size());
 		}
+
+		static void writeWord(std::vector<uint8_t>& _dst, dsp56k::TWord _word);
+
 	private:
 		size_t m_existingDataSize = 0;
+	};
+
+	class AsyncWriter
+	{
+	public:
+		AsyncWriter(std::string _filename, uint32_t _samplerate, bool _measureSilence = false);
+		~AsyncWriter();
+
+		void append(const std::function<void(std::vector<dsp56k::TWord>&)>& _func);
+
+		void setFinished()
+		{
+			m_finished = true;
+		}
+
+		bool isFinished() const
+		{
+			return m_finished;
+		}
+
+		uint32_t getSilenceDuration() const
+		{
+			return m_silenceDuration;
+		}
+
+	private:
+		void threadWriteFunc();
+
+		const std::string m_filename;
+		const uint32_t m_samplerate;
+		const bool m_measureSilence;
+
+		bool m_finished = false;
+		std::unique_ptr<std::thread> m_thread;
+		uint32_t m_silenceDuration = 0;
+		std::mutex m_writeMutex;
+		std::vector<dsp56k::TWord> m_stereoOutput;
 	};
 };
