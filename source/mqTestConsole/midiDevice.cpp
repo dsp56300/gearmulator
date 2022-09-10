0570ef5c21f19ccdbb852f626b09afc90ea80fe4
dsp56300 <dsp56300@users.noreply.github.com>
2022-09-10 21:58:36 +0200

    android compatibility

--- source/mqTestConsole/midiDevice.cpp
+++ source/mqTestConsole/midiDevice.cpp
@@ -2,6 +2,66 @@
 
 #include "../portmidi/pm_common/portmidi.h"
 
+#ifdef ANDROID
+extern "C"
+{
+	// nop implementation of PortMidi
+
+	PmError Pm_Initialize( void )
+	{
+		return pmHostError;
+	}
+	const PmDeviceInfo* Pm_GetDeviceInfo( PmDeviceID id)
+	{
+		return nullptr;
+	}
+	int Pm_CountDevices( void )
+	{
+		return 0;
+	}
+	PmDeviceID Pm_GetDefaultInputDeviceID( void )
+	{
+		return 0;
+	}
+	PmDeviceID Pm_GetDefaultOutputDeviceID( void )
+	{
+		return 0;
+	}
+	PmError Pm_Close( PortMidiStream* stream )
+	{
+		return pmNoError;
+	}
+	PmError Pm_WriteSysEx(PortMidiStream *stream, PmTimestamp when, unsigned char *msg)
+	{
+		return pmHostError;
+	}
+	const char *Pm_GetErrorText( PmError errnum )
+	{
+		return "Platform not supported";
+	}
+	PmError Pm_Write( PortMidiStream *stream, PmEvent *buffer, int32_t length)
+	{
+		return pmHostError;
+	}
+	PmError Pm_OpenOutput(PortMidiStream** stream, PmDeviceID outputDevice, void *outputDriverInfo,int32_t bufferSize, PmTimeProcPtr time_proc, void *time_info, int32_t latency )
+	{
+		return pmHostError;
+	}
+	PmError Pm_Poll( PortMidiStream *stream )
+	{
+		return pmNoData;
+	}
+	int Pm_Read(PortMidiStream *stream, PmEvent *buffer, int32_t length)
+	{
+		return 0;
+	}
+	PmError Pm_OpenInput(PortMidiStream** stream, PmDeviceID inputDevice, void *inputDriverInfo, int32_t bufferSize, PmTimeProcPtr time_proc, void *time_info)
+	{
+		return pmHostError;
+	}
+}
+#endif
+
 MidiDevice::MidiDevice(const std::string& _deviceName, bool _output): Device(_deviceName), m_output(_output)
 {
 	Pm_Initialize();
