0c45833001fe8c60346816a0d2213fbf896bf5a6
dsp56300 <dsp56300@users.noreply.github.com>
2022-09-10 21:56:47 +0200

    rename android build scripts

--- build_cmake_android.bat
+++
@@ -1,5 +0,0 @@
-REM call build_cmake_android_abi.bat armeabi-v7a
-REM call build_cmake_android_abi.bat "armeabi-v7a with NEON"
-call build_cmake_android_abi.bat arm64-v8a
-REM call build_cmake_android_abi.bat x86
-REM call build_cmake_android_abi.bat x86_64
