b9c48fef558d44e2c3341895b7c3fb54d22d650d
dsp56300 <dsp56300@users.noreply.github.com>
2022-09-17 15:13:00 +0200

    implement am29f sector erase

--- source/mqLib/am29f.cpp
+++ source/mqLib/am29f.cpp
@@ -2,6 +2,8 @@
 
 #include <cassert>
 
+#include "dsp56kEmu/logging.h"
+
 Am29f::Am29f(uint8_t* _buffer, const size_t _size, bool _useWriteEnable, bool _bitreversedCmdAddr): m_buffer(_buffer), m_size(_size), m_useWriteEnable(_useWriteEnable), m_bitreverseCmdAddr(_bitreversedCmdAddr)
 {
 	auto br = [&](uint16_t x)
@@ -72,20 +74,51 @@ void Am29f::write(const uint32_t _addr, const uint16_t _data)
 	}
 }
 
-void Am29f::execCommand(const CommandType _command, const uint32_t _addr, const uint16_t _data)
+void Am29f::execCommand(const CommandType _command, uint32_t _addr, const uint16_t _data)
 {
 	switch (_command)
 	{
 	case CommandType::ChipErase:
 		assert(false);
 		break;
-	case CommandType::SectorErase: 
-		assert(false);
+	case CommandType::SectorErase:
+		{
+			size_t sectorSizekB = 0;
+			switch (_addr)
+			{
+				case 0x00000:	sectorSizekB = 16;	break;
+				case 0x04000:
+				case 0x06000:	sectorSizekB = 8;	break;
+				case 0x08000:	sectorSizekB = 32;	break;
+				case 0x10000:
+				case 0x20000:
+				case 0x30000:
+				case 0x40000:
+				case 0x50000:
+				case 0x60000:
+				case 0x70000:	sectorSizekB = 64;	break;
+				case 0x78000:	
+				case 0x7A000:	
+				case 0x7C000:
+					// mq sends erase commands for a flash with top boot block even though a chip with bottom boot block is installed
+					_addr = 0x70000;
+					sectorSizekB = 64;
+					break;
+			default:
+				LOG("Unable to erase sector at " << HEX(_addr) << ", out of bounds!");
+				return;
+			}
+
+			LOG("Erasing Sector at " << HEX(_addr) << ", size " << HEX(1024 * sectorSizekB));
+			for(size_t i = _addr; i< _addr + sectorSizekB * 1024; ++i)
+				m_buffer[i] = 0xff;
+		}
 		break;
 	case CommandType::Program:
 		{
 			if(_addr >= m_size)
 				return;
+			LOG("Programming word at " << HEX(_addr) << ", value " << HEXN(_data, 4));
 			uint8_t* p = &m_buffer[_addr];
 			auto* p16 = reinterpret_cast<uint16_t*>(p);
 			*p16 = _data;
