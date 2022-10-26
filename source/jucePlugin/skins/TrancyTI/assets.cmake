5bff7d66db17b882e5b7837b8b4f955c9416fb9b
dsp56300 <dsp56300@users.noreply.github.com>
2022-10-27 00:24:23 +0200

    upgrade Trancy TI ui

--- source/jucePlugin/skins/TrancyTI/assets.cmake
+++ source/jucePlugin/skins/TrancyTI/assets.cmake
@@ -1,6 +1,38 @@
 set(ASSETS_VirusTI_Trancy
 	${CMAKE_CURRENT_LIST_DIR}/background.png
 	${CMAKE_CURRENT_LIST_DIR}/button_1.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_1.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_10.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_11.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_12.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_13.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_14.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_15.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_16.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_17.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_18.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_19.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_2.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_20.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_21.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_22.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_23.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_24.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_25.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_26.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_27.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_28.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_29.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_3.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_30.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_31.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_32.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_4.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_5.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_6.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_7.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_8.png
+	${CMAKE_CURRENT_LIST_DIR}/button_arp_9.png
 	${CMAKE_CURRENT_LIST_DIR}/button_channel_1.png
 	${CMAKE_CURRENT_LIST_DIR}/button_channel_10.png
 	${CMAKE_CURRENT_LIST_DIR}/button_channel_11.png
@@ -17,6 +49,9 @@ set(ASSETS_VirusTI_Trancy
 	${CMAKE_CURRENT_LIST_DIR}/button_channel_7.png
 	${CMAKE_CURRENT_LIST_DIR}/button_channel_8.png
 	${CMAKE_CURRENT_LIST_DIR}/button_channel_9.png
+	${CMAKE_CURRENT_LIST_DIR}/button_env_3.png
+	${CMAKE_CURRENT_LIST_DIR}/button_env_4.png
+	${CMAKE_CURRENT_LIST_DIR}/button_env_filter.png
 	${CMAKE_CURRENT_LIST_DIR}/button_load.png
 	${CMAKE_CURRENT_LIST_DIR}/button_menu.png
 	${CMAKE_CURRENT_LIST_DIR}/button_mono_poly.png
@@ -26,9 +61,9 @@ set(ASSETS_VirusTI_Trancy
 	${CMAKE_CURRENT_LIST_DIR}/button_prev.png
 	${CMAKE_CURRENT_LIST_DIR}/button_save.png
 	${CMAKE_CURRENT_LIST_DIR}/button_single.png
-	${CMAKE_CURRENT_LIST_DIR}/button_slow_fast.png
 	${CMAKE_CURRENT_LIST_DIR}/button_sub.png
 	${CMAKE_CURRENT_LIST_DIR}/Digital.ttf
+	${CMAKE_CURRENT_LIST_DIR}/dummy.png
 	${CMAKE_CURRENT_LIST_DIR}/knob_big.png
 	${CMAKE_CURRENT_LIST_DIR}/knob_mid.png
 	${CMAKE_CURRENT_LIST_DIR}/knob_small.png
@@ -53,22 +88,33 @@ set(ASSETS_VirusTI_Trancy
 	${CMAKE_CURRENT_LIST_DIR}/panel_3_osc_wave_pwm.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_3_osc_wavetable.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_4_filter.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_4_filter_env_filter.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_5_lfo.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_6_matrix.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_7_arp.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_0.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_1.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_2.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_3.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_4.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_5.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_chorus_air_chorus.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_chorus_classic.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_chorus_hyper_chorus.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_chorus_off.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_chorus_rotary_speaker.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_chorus_vibrato.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_chorus_vintage.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_filterbank_frequency_shift.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_filterbank_off.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_filterbank_pole_xfade.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_filterbank_ringmodulator.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_filterbank_vari_slopes.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_filterbank_vowel_filter.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_8_fx1_rotary_speaker.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_9_fx2.png
+	${CMAKE_CURRENT_LIST_DIR}/panel_9_fx2_delay_classic.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_9_fx2_delay_tape.png
 	${CMAKE_CURRENT_LIST_DIR}/slider_knob.png
 
