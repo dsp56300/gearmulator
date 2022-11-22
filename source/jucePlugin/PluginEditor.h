b497190eeecac828754a00beb36149985d60421f
dreddi <dreddi@users.noreply.github.com>
2022-11-22 16:33:28 +1100

    fix: #102, plugin does not recall the user's chosen GUI scale correctly in Bitwig Studio.

--- source/jucePlugin/PluginEditor.h
+++ source/jucePlugin/PluginEditor.h
@@ -14,6 +14,7 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 	void mouseDown(const juce::MouseEvent& event) override;
+    float getRootScale() const;
 
 private:
 	struct Skin
