245b8bb386a15d86662b6daa2e2c69dad4707ca7
dsp56300 <dsp56300@users.noreply.github.com>
2022-11-25 21:26:28 +0100

    fix device keeping silent because presets are sent too fast

--- source/virusLib/microcontroller.cpp
+++ source/virusLib/microcontroller.cpp
@@ -36,7 +36,7 @@ constexpr uint8_t
 						92,  93,  94,  95,  96,  97,  98,  99,  105, 106, 110, 111, 112, 113,
 						114, 115, 116, 117, 118, 120, 121, 122, 123, 124, 125, 126, 127};
 
-constexpr int g_presetWriteDelaySamples = 256;
+constexpr int g_presetWriteDelaySamples = 512;
 
 namespace virusLib
 {
