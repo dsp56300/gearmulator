fa1095a3136121dfb06c8091ed9ad8f64ce046e9
dsp56300 <dsp56300@users.noreply.github.com>
2022-12-05 19:41:26 +0100

    fix warning

--- source/jucePlugin/ui3/VirusEditor.cpp
+++ source/jucePlugin/ui3/VirusEditor.cpp
@@ -433,14 +433,14 @@ namespace genericVirusUI
 		}
 
 		juce::PopupMenu banksMenu;
-		for(auto b=0; b<getController().getBankCount(); ++b)
+		for(uint8_t b=0; b<static_cast<uint8_t>(getController().getBankCount()); ++b)
 		{
             std::stringstream bankName;
             bankName << "Bank " << static_cast<char>('A' + b);
 
 			addEntry(banksMenu, bankName.str(), [this, b](const FileType _type)
 			{
-				savePresets(SaveType::Bank, _type, static_cast<uint8_t>(b));
+				savePresets(SaveType::Bank, _type, b);
 			});
 		}
 
