9b4580ec35ba4529974dcbc2b079c2e410d188ce
dsp56300 <dsp56300@users.noreply.github.com>
2022-12-28 23:29:25 +0100

    embed default GUI

---
+++ source/mqJucePlugin/skins/mqDefault/assets.cmake
@@ -0,0 +1,11 @@
+set(ASSETS_mqDefault
+	${CMAKE_CURRENT_LIST_DIR}/bakedBG.png
+	${CMAKE_CURRENT_LIST_DIR}/Digital.ttf
+	${CMAKE_CURRENT_LIST_DIR}/encoder_ranged.png
+	${CMAKE_CURRENT_LIST_DIR}/ledcircle_7.png
+	${CMAKE_CURRENT_LIST_DIR}/ledcircle_9.png
+	${CMAKE_CURRENT_LIST_DIR}/pageOsc.png
+	${CMAKE_CURRENT_LIST_DIR}/toggleWithLED.png
+
+	${CMAKE_CURRENT_LIST_DIR}/mqDefault.json
+)
