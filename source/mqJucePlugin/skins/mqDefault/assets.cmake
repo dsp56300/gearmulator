43f759cd11dc2314fa2c76e3e21f6e9e02827125
dsp56300 <dsp56300@users.noreply.github.com>
2022-12-29 01:53:20 +0100

    default gui update

--- source/mqJucePlugin/skins/mqDefault/assets.cmake
+++ source/mqJucePlugin/skins/mqDefault/assets.cmake
@@ -1,10 +1,12 @@
 set(ASSETS_mqDefault
-	${CMAKE_CURRENT_LIST_DIR}/bakedBG.png
 	${CMAKE_CURRENT_LIST_DIR}/Digital.ttf
 	${CMAKE_CURRENT_LIST_DIR}/encoder_ranged.png
+	${CMAKE_CURRENT_LIST_DIR}/led.png
 	${CMAKE_CURRENT_LIST_DIR}/ledcircle_7.png
 	${CMAKE_CURRENT_LIST_DIR}/ledcircle_9.png
+	${CMAKE_CURRENT_LIST_DIR}/mqDefaultBG.png
 	${CMAKE_CURRENT_LIST_DIR}/pageOsc.png
+	${CMAKE_CURRENT_LIST_DIR}/pagePatchBrowser.png
 	${CMAKE_CURRENT_LIST_DIR}/toggleWithLED.png
 
 	${CMAKE_CURRENT_LIST_DIR}/mqDefault.json
