09ef76d50ca72ac9a569f67c7a5814abb9be4067
dsp56300 <dsp56300@users.noreply.github.com>
2023-01-10 23:07:12 +0100

    rclone to upload now uses temporary file name and then does a server-side rename

--- scripts/rclone.cmake
+++ scripts/rclone.cmake
@@ -10,16 +10,37 @@ if(NOT RCLONE_CONF)
 	endif()
 endif()
 
+set(postfix "_partial")
+
 macro(copyArtefacts TARGET FOLDER)
 	set(RCLONE_RESULT 0)
-	execute_process(COMMAND rclone --config ${RCLONE_CONF} copy 
-		--transfers=1 -v --include *.zip --include *.deb --include *.rpm --min-size 100 --max-depth 1 "${ROOT_DIR}" "${TARGET}/${FOLDER}"
-		COMMAND_ECHO STDOUT
-		RESULT_VARIABLE RCLONE_RESULT
-		WORKING_DIRECTORY ${ROOT_DIR})
-	if(RCLONE_RESULT)
-		message(FATAL_ERROR "Failed to execute rclone: " ${CMD_RESULT})
-	endif()
+	file(GLOB files LIST_DIRECTORIES false "${ROOT_DIR}/*.zip" "${ROOT_DIR}/*.deb" "${ROOT_DIR}/*.rpm")
+	foreach(fileLocal ${files})
+		
+		file(SIZE ${fileLocal} fileSize)
+		if(${fileSize} GREATER 100)
+			file(RELATIVE_PATH remoteFile "${ROOT_DIR}" ${fileLocal})
+
+			file(RELATIVE_PATH remoteFile "${ROOT_DIR}" ${fileLocal})
+			set(remoteFileTemp "${remoteFile}${postfix}")
+
+			set(RCLONE_RESULT 0)
+			execute_process(COMMAND rclone -v --config ${RCLONE_CONF} copyto 
+				"${fileLocal}" "${TARGET}/${FOLDER}/${remoteFileTemp}"
+				COMMAND_ECHO STDOUT RESULT_VARIABLE RCLONE_RESULT WORKING_DIRECTORY ${ROOT_DIR})
+			if(RCLONE_RESULT)
+				message(FATAL_ERROR "Failed to execute rclone: " ${CMD_RESULT})
+			endif()
+
+			set(RCLONE_RESULT 0)
+			execute_process(COMMAND rclone -v --config ${RCLONE_CONF} moveto 
+				"${TARGET}/${FOLDER}/${remoteFileTemp}" "${TARGET}/${FOLDER}/${remoteFile}"
+				COMMAND_ECHO STDOUT RESULT_VARIABLE RCLONE_RESULT WORKING_DIRECTORY ${ROOT_DIR})
+			if(RCLONE_RESULT)
+				message(FATAL_ERROR "Failed to execute rclone: " ${CMD_RESULT})
+			endif()
+		endif()
+	endforeach()
 endmacro()
 
 macro(copyDataFrom FROM TO)
