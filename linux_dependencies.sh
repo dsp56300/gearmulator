267b69174b293917f5f36b148f703848b3b8d202
dsp56300 <dsp56300@users.noreply.github.com>
2023-03-23 20:10:26 +0100

    update linux dependencies

--- linux_dependencies.sh
+++ linux_dependencies.sh
@@ -1,3 +1,3 @@
-sudo apt-get -y install gcc g++ cmake rpm
+sudo apt-get -y install gcc g++ cmake rpm git
 sudo apt-get -y install libfreetype6-dev libx11-dev libxinerama-dev libxrandr-dev libxcursor-dev mesa-common-dev libasound2-dev freeglut3-dev libxcomposite-dev
 sudo apt-get -y install pkg-config
