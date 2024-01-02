75ff0ee05d5ac8d99dd8e793d1b9a748f0b58bba
dsp56300 <dsp56300@users.noreply.github.com>
2024-01-02 23:29:43 +0100

    begin state implementation

--- source/jucePluginLib/patchdb/db.h
+++ source/jucePluginLib/patchdb/db.h
@@ -11,6 +11,7 @@
 #include "search.h"
 
 #include "jobqueue.h"
+#include "state.h"
 
 #include "juce_core/juce_core.h"
 
@@ -124,5 +125,8 @@ namespace pluginLib::patchDB
 		std::unordered_map<uint32_t, std::shared_ptr<Search>> m_searches;
 		std::unordered_set<SearchHandle> m_cancelledSearches;
 		uint32_t m_nextSearchHandle = 0;
+
+		// current plugin state
+		State m_state;
 	};
 }
