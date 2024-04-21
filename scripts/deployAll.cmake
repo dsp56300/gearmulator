a3a04618796d9055f2c7c6eec89f7067d6ea89bb
dsp56300 <dsp56300@users.noreply.github.com>
2024-04-21 11:38:12 +0200

    fix upload folder name was reversed

--- scripts/deployAll.cmake
+++ scripts/deployAll.cmake
@@ -10,7 +10,7 @@ endif()
 
 macro(deploySynth NAME)
 	message(STATUS "Deploying synth ${${NAME}_name}")
-	set(uploadFolder ${FOLDER}/${${NAME}_folder})
+	set(uploadFolder ${${NAME}_folder}/${FOLDER})
 	set(filter "${${NAME}_name}")
 	execute_process(COMMAND cmake -DFOLDER=${uploadFolder} -DFILTER=${filter} -DUPLOAD_LOCAL=${UPLOAD_LOCAL} -DUPLOAD_REMOTE=${UPLOAD_REMOTE} -P ${CMAKE_CURRENT_LIST_DIR}/deploy.cmake COMMAND_ECHO STDOUT WORKING_DIRECTORY ${gearmulator_BINARY_DIR} COMMAND_ERROR_IS_FATAL ANY)
 endmacro()
