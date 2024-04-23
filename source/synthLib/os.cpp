b1dc595eb64a1c03456aff3e18fefbb80252de80
dsp56300 <dsp56300@users.noreply.github.com>
2024-04-23 21:11:22 +0200

    add warning if running in Rosetta

--- source/synthLib/os.cpp
+++ source/synthLib/os.cpp
@@ -30,6 +30,11 @@
 #include <immintrin.h>
 #endif
 
+#ifdef __APPLE__
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#endif
+
 namespace synthLib
 {
     std::string getModulePath(bool _stripPluginComponentFolders/* = true*/)
@@ -390,4 +395,21 @@ namespace synthLib
 		return fopen(_name.c_str(), _mode);
 #endif
     }
+
+    bool isRunningUnderRosetta()
+    {
+#ifdef __APPLE__
+		int ret = 0;
+		size_t size = sizeof(ret);
+		if (sysctlbyname("sysctl.proc_translated", &ret, &size, NULL, 0) == -1) 
+		{
+			if (errno == ENOENT)
+				return false;	// no, native
+			return false;		// unable to tell, assume native
+		}
+		return ret == 1;		// Rosetta if result is 1
+#else
+		return false;
+#endif
+    	}
 } // namespace synthLib
