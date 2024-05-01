f9c80cd46a766899d5bf1832c346869dbad2be7e
dsp56300 <dsp56300@users.noreply.github.com>
2024-05-01 13:48:43 +0200

    fix some warnings

--- source/synthLib/romLoader.cpp
+++ source/synthLib/romLoader.cpp
@@ -9,16 +9,16 @@ namespace synthLib
 	{
 		std::vector<std::string> results;
 
-        const auto path = synthLib::getModulePath();
+        const auto path = getModulePath();
 		synthLib::findFiles(results, path, _extension, _minSize, _maxSize);
 
-    	const auto path2 = synthLib::getModulePath(false);
+    	const auto path2 = getModulePath(false);
 		if(path2 != path)
 			synthLib::findFiles(results, path2, _extension, _minSize, _maxSize);
 
 		if(results.empty())
 		{
-            const auto path3 = synthLib::getCurrentDirectory();
+            const auto path3 = getCurrentDirectory();
 			if(path3 != path2 && path3 != path)
 				synthLib::findFiles(results, path, _extension, _minSize, _maxSize);
 		}
@@ -26,7 +26,7 @@ namespace synthLib
 		return results;
 	}
 
-	std::vector<std::string> RomLoader::findFiles(const std::string& _path, const std::string& _extension, size_t _minSize, size_t _maxSize)
+	std::vector<std::string> RomLoader::findFiles(const std::string& _path, const std::string& _extension, const size_t _minSize, const size_t _maxSize)
 	{
 		if(_path.empty())
 			return findFiles(_extension, _minSize, _maxSize);
