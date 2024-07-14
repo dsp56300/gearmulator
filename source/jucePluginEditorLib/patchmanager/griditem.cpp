e878f1f85bcfe3177f7cca3ace2d98716e8be5f5
dsp56300 <dsp56300@users.noreply.github.com>
2024-07-14 15:15:12 +0200

    remove black border

--- source/jucePluginEditorLib/patchmanager/griditem.cpp
+++ source/jucePluginEditorLib/patchmanager/griditem.cpp
@@ -7,16 +7,13 @@ namespace jucePluginEditorLib::patchManager
 	GridItem::GridItem(Grid& _grid) : m_grid(_grid)
 	{
 		setInterceptsMouseClicks(false, true);
-		setSize(_grid.getItemWidth(), _grid.getItemHeight());
+		setSize(static_cast<int>(_grid.getItemWidth()), static_cast<int>(_grid.getItemHeight()));
 	}
 
 	void GridItem::paint(juce::Graphics& _g)
 	{
 		if(m_index >= static_cast<uint32_t>(m_grid.getNumRows()))
 			return;
-
-		_g.setColour(m_grid.findColour(juce::ListBox::backgroundColourId).withMultipliedBrightness(0.1f));
-		_g.drawRect(0, 0, getWidth()-1, getHeight()-1);
 
 		m_grid.paintListBoxItem(static_cast<int>(m_index), _g, getWidth(), getHeight(), m_grid.isSelected(m_index));
 
