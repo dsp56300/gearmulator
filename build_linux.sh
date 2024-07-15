7184c76a1f887637fd6b2c9efe44fc336fca5bcc
dsp56300 <dsp56300@users.noreply.github.com>
2024-07-15 20:01:17 +0200

    add hostname to build output folder

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,15 @@
-cmake . -B ./temp/cmake_linux -Dgearmulator_BUILD_JUCEPLUGIN=ON -Dgearmulator_BUILD_JUCEPLUGIN_CLAP=ON -Dgearmulator_BUILD_JUCEPLUGIN_LV2=ON -Dgearmulator_SYNTH_OSIRUS=ON
-cd ./temp/cmake_linux
-cmake --build . --config Release
+host=$(hostname)
+
+if [ -z "$1" ]
+then
+	threads=4
+else
+	threads=$1
+fi
+
+cmake . -B ./temp/cmake_${host} -Dgearmulator_BUILD_JUCEPLUGIN=ON -Dgearmulator_BUILD_JUCEPLUGIN_CLAP=ON -Dgearmulator_BUILD_JUCEPLUGIN_LV2=ON -Dgearmulator_SYNTH_OSIRUS=ON
+cd ./temp/cmake_${host}
+cmake --build . --config Release -j $threads
 cpack -G DEB
 cpack -G RPM
 cpack -G ZIP
