df6fc70be64b0f63980fd358d21a52ebb70da47d
dsp56300 <dsp56300@users.noreply.github.com>
2024-08-05 22:18:29 +0200

    implement arp state/button

--- source/nord/n2x/n2xJucePlugin/n2xArp.cpp
+++ source/nord/n2x/n2xJucePlugin/n2xArp.cpp
@@ -1,8 +1,51 @@
 #include "n2xArp.h"
 
+#include "n2xController.h"
+#include "n2xEditor.h"
+
+#include "juce_gui_basics/juce_gui_basics.h"
+
 namespace n2xJucePlugin
 {
-	Arp::Arp(Editor& _editor) : m_editor(_editor)
+	Arp::Arp(Editor& _editor) : m_editor(_editor), m_btArpActive(_editor.findComponentT<juce::Button>("ArpEnabled"))
 	{
+		auto& c = _editor.getN2xController();
+
+		m_onCurrentPartChanged.set(c.onCurrentPartChanged, [this](const uint8_t&)
+		{
+			bind();
+		});
+
+		m_btArpActive->onClick = [this]
+		{
+			if(!m_param)
+				return;
+			m_param->setUnnormalizedValueNotifyingHost(m_btArpActive->getToggleState() ? 0 : 3, pluginLib::Parameter::Origin::Ui);
+		};
+
+		bind();
+	}
+
+	void Arp::bind()
+	{
+		const auto& c = m_editor.getN2xController();
+
+		m_param = c.getParameter("Lfo2Dest", c.getCurrentPart());
+
+		m_onParamChanged.set(m_param, [this](const pluginLib::Parameter* _parameter)
+		{
+			updateStateFromParameter(_parameter);
+		});
+
+		updateStateFromParameter(m_param);
+	}
+
+	void Arp::updateStateFromParameter(const pluginLib::Parameter* _parameter) const
+	{
+		const auto v = _parameter->getUnnormalizedValue();
+
+		const bool arpActive = v != 3 && v != 4 && v != 7;
+
+		m_btArpActive->setToggleState(arpActive, juce::dontSendNotification);
 	}
 }
--- source/nord/n2x/n2xJucePlugin/n2xArp.h
+++ source/nord/n2x/n2xJucePlugin/n2xArp.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "jucePluginLib/parameterlistener.h"
+
+namespace juce
+{
+	class Button;
+}
+
 namespace n2xJucePlugin
 {
 	class Editor;
@@ -10,6 +17,13 @@ namespace n2xJucePlugin
 		explicit Arp(Editor& _editor);
 
 	private:
+		void bind();
+		void updateStateFromParameter(const pluginLib::Parameter* _parameter) const;
+
 		Editor& m_editor;
+		juce::Button* m_btArpActive;
+		pluginLib::ParameterListener m_onParamChanged;
+		pluginLib::EventListener<uint8_t> m_onCurrentPartChanged;
+		pluginLib::Parameter* m_param = nullptr;
 	};
 }
