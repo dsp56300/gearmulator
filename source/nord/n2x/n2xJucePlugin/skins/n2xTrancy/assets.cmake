20e99c40b7af834e4a4b9b968550fc76b6e04a9d
dsp56300 <dsp56300@users.noreply.github.com>
2024-08-10 13:21:30 +0200

    UI update

--- source/nord/n2x/n2xJucePlugin/skins/n2xTrancy/assets.cmake
+++ source/nord/n2x/n2xJucePlugin/skins/n2xTrancy/assets.cmake
@@ -1,8 +1,12 @@
 set(ASSETS_n2xTrancy
 	${CMAKE_CURRENT_LIST_DIR}/background.png
+	${CMAKE_CURRENT_LIST_DIR}/button_a.png
+	${CMAKE_CURRENT_LIST_DIR}/button_b.png
 	${CMAKE_CURRENT_LIST_DIR}/button_black.png
 	${CMAKE_CURRENT_LIST_DIR}/button_browser.png
-	${CMAKE_CURRENT_LIST_DIR}/button_extra.png
+	${CMAKE_CURRENT_LIST_DIR}/button_c.png
+	${CMAKE_CURRENT_LIST_DIR}/button_d.png
+#	${CMAKE_CURRENT_LIST_DIR}/button_extra.png
 	${CMAKE_CURRENT_LIST_DIR}/button_main.png
 	${CMAKE_CURRENT_LIST_DIR}/button_red.png
 	${CMAKE_CURRENT_LIST_DIR}/button_round.png
@@ -12,7 +16,7 @@ set(ASSETS_n2xTrancy
 	${CMAKE_CURRENT_LIST_DIR}/panel_1_main_arpeggiator.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_1_main_lfo2.png
 	${CMAKE_CURRENT_LIST_DIR}/panel_2_browser.png
-	${CMAKE_CURRENT_LIST_DIR}/panel_3_extra.png
+#	${CMAKE_CURRENT_LIST_DIR}/panel_3_extra.png
 
 	${CMAKE_CURRENT_LIST_DIR}/n2xTrancy.json
 )
