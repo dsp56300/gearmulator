e68cfc5f5c8b0a438f3064c326474eeed438d244
dsp56300 <dsp56300@users.noreply.github.com>
2024-10-09 16:51:05 +0200

    remove massive output logging when checkking for IPO

--- base.cmake
+++ base.cmake
@@ -67,11 +67,11 @@ else()
 
 	include(CheckIPOSupported)
 
-	check_ipo_supported(RESULT result OUTPUT output)
+	check_ipo_supported(RESULT result)
 	if(result)
 		set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
 	else()
-		message(WARNING "IPO is not supported: ${output}")
+		message(WARNING "IPO is not supported")
 	endif()
 	
 	string(APPEND CMAKE_C_FLAGS_RELEASE " -Ofast -fno-stack-protector")
