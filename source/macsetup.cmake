f23b4a96627152e2a78d05aaaee09883d6b0bfb0
dsp56300 <dsp56300@users.noreply.github.com>
2024-10-26 22:58:58 +0200

    rename mac setup script to contain the name of the product

--- source/macsetup.cmake
+++ source/macsetup.cmake
@@ -2,7 +2,7 @@ set(MACSETUP_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR})
 
 macro(createMacSetupScript productName)
 	set(MAC_SETUP_PRODUCT_NAME ${productName})
-	set(macSetupFile ${CMAKE_CURRENT_BINARY_DIR}/macsetup.command)
+	set(macSetupFile ${CMAKE_CURRENT_BINARY_DIR}/macsetup_${productName}.command)
 	configure_file(${MACSETUP_SOURCE_DIR}/macsetup.command.in ${macSetupFile})
 endmacro()
 
