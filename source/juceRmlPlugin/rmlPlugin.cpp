82c2b303cb49f6d681dc6cc441119a6e3cf6469e
dsp56300 <dsp56300@users.noreply.github.com>
2025-06-15 23:00:28 +0200

    add support for placeholders for text inputs

--- source/juceRmlPlugin/rmlPlugin.cpp
+++ source/juceRmlPlugin/rmlPlugin.cpp
@@ -3,9 +3,11 @@
 #include "rmlParameterBinding.h"
 
 #include "juceRmlUi/rmlElemComboBox.h"
+#include "juceRmlUi/rmlEventListener.h"
 
 #include "RmlUi/Core/Core.h"
 #include "RmlUi/Core/Element.h"
+#include "RmlUi/Core/Elements/ElementFormControlInput.h"
 
 namespace rmlPlugin
 {
@@ -44,30 +46,52 @@ namespace rmlPlugin
 
 		auto* attrib = _element->GetAttribute("param");
 
-		if (!attrib)
-			return;
-
-		const auto name = attrib->Get<Rml::String>();
-		const auto param = RmlParameterRef::createVariableName(name);
-
-		_element->SetAttribute("data-attr-min", param + "_min");
-		_element->SetAttribute("data-attr-max", param + "_max");
-		_element->SetAttribute("data-value", param + "_value");
-
-		if (auto* combo = dynamic_cast<juceRmlUi::ElemComboBox*>(_element))
+		if (attrib)
 		{
-			std::vector<Rml::String> options;
-			auto* p = m_controller.getParameter(name, 0);	// TODO: which part?
+			const auto name = attrib->Get<Rml::String>();
+			const auto param = RmlParameterRef::createVariableName(name);
 
-			if (!p)
+			_element->SetAttribute("data-attr-min", param + "_min");
+			_element->SetAttribute("data-attr-max", param + "_max");
+			_element->SetAttribute("data-value", param + "_value");
+
+			if (auto* combo = dynamic_cast<juceRmlUi::ElemComboBox*>(_element))
 			{
-				std::stringstream ss;
-				ss << "Failed to find parameter " << name << " for combo box";
-				Rml::Log::Message(Rml::Log::LT_ERROR, ss.str().c_str());
-				return;
-			}
+				std::vector<Rml::String> options;
+				auto* p = m_controller.getParameter(name, 0);	// TODO: which part?
 
-			combo->setOptions(p->getDescription().valueList.texts);
+				if (!p)
+				{
+					std::stringstream ss;
+					ss << "Failed to find parameter " << name << " for combo box";
+					Rml::Log::Message(Rml::Log::LT_ERROR, ss.str().c_str());
+					return;
+				}
+
+				combo->setOptions(p->getDescription().valueList.texts);
+			}
+		}
+
+		if (auto* input = dynamic_cast<Rml::ElementFormControlInput*>(_element))
+		{
+			// placeholder for input elements, only valid if an input of type text has exacly one child.
+			// if there is a placeholder, it is only shown when the input is empty.
+//			if (input->GetNumChildren() == 1)
+			{
+				auto type = input->GetAttribute("type");
+
+				if (type && type->Get<Rml::String>() == "text")
+				{
+					juceRmlUi::EventListener::Add(input, Rml::EventId::Change, [input](const Rml::Event&)
+					{
+						const auto isEmpty = input->GetValue().empty();
+						if (auto child = input->GetFirstChild())
+						{
+							child->SetProperty(Rml::PropertyId::Visibility, Rml::Property(isEmpty ? Rml::Style::Visibility::Visible :  Rml::Style::Visibility::Hidden));
+						}
+					});
+				}
+			}
 		}
 	}
 }
