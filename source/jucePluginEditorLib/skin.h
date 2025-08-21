0d8d4975139bb78eb1520fcc0bfeb2ddfdb0acb3
dsp56300 <dsp56300@users.noreply.github.com>
2025-08-21 20:59:00 +0200

    get rid of assets.cmake, skin files are collected via cmake glob and they are added as c++ list to integrated skins

--- source/jucePluginEditorLib/skin.h
+++ source/jucePluginEditorLib/skin.h
@@ -6,9 +6,10 @@ namespace jucePluginEditorLib
 {
 	struct Skin
 	{
-		std::string displayName;	// == folder name on disk if not embedded into binary
-		std::string filename;		// name of the skin file, e.g. "skin.json" (old) or "skin.rml" (new/converted)
-		std::string folder;			// empty if skin is embedded into binary
+		std::string displayName;		// == folder name on disk if not embedded into binary
+		std::string filename;			// name of the skin file, e.g. "skin.json" (old) or "skin.rml" (new/converted)
+		std::string folder;				// empty if skin is embedded into binary
+		std::vector<std::string> files;	// only valid for embedded skins, contains all files that are part of the skin
 
 		bool operator == (const Skin& _other) const
 		{
--- source/skins.cmake
+++ source/skins.cmake
@@ -1,15 +1,35 @@
 set(SkinsHeaderDir ${CMAKE_CURRENT_LIST_DIR})
 
-macro(addSkin productName skinName skinFolder skinJson assetName)
-	include(${skinFolder}/assets.cmake)
+macro(addSkin productName skinName skinFolder skinRootFile)
+	file(GLOB SKIN_FILES
+		RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}
+		"${skinFolder}/*.json"	# we keep these for now, can be removed once all skins are converted to RML
+		"${skinFolder}/*.png"
+		"${skinFolder}/*.rml"
+		"${skinFolder}/*.rcss"
+	)
+	
+	message(STATUS "Skin files for ${skinFolder}: ${SKIN_FILES}")
+	message(STATUS "Path test: " ${CMAKE_CURRENT_SOURCE_DIR})
 
-	if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${skinFolder}/${skinJson})
-		message(FATAL_ERROR "${CMAKE_CURRENT_SOURCE_DIR}/${skinFolder}/${skinJson} not found")
+	if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${skinFolder}/${skinRootFile})
+		message(FATAL_ERROR "${CMAKE_CURRENT_SOURCE_DIR}/${skinFolder}/${skinRootFile} not found")
 	endif()
 
-	list(APPEND ASSETS_SKINS ${${assetName}})
+	list(APPEND ASSETS_SKINS ${SKIN_FILES})
 
-	string(JOIN ", " SKIN_CPP_ENTRIES ${SKIN_CPP_ENTRIES} "{\"${skinName}\", \"${skinJson}\", \"\"}")
+	set(SKIN_FILENAMES "")
+	foreach(f ${SKIN_FILES})
+	    get_filename_component(fname "${f}" NAME)
+		set(fname "\"${fname}\"")
+		if(SKIN_FILENAMES STREQUAL "")
+		    set(SKIN_FILENAMES ${fname})
+		else()
+			set(SKIN_FILENAMES "${SKIN_FILENAMES}, ${fname}")
+		endif()
+	endforeach()
+
+	string(JOIN ", " SKIN_CPP_ENTRIES ${SKIN_CPP_ENTRIES} "{\"${skinName}\", \"${skinRootFile}\", \"\", {${SKIN_FILENAMES}}}")
 endmacro()
 
 macro(buildSkinHeader)
