8d14e962feb8d8077bcde8117485654f90f3b1b7
dsp56300 <dsp56300@users.noreply.github.com>
2025-08-23 21:29:22 +0200

    convert LCD to rmlUi

--- source/jucePluginEditorLib/lcd.cpp
+++ source/jucePluginEditorLib/lcd.cpp
@@ -1,5 +1,8 @@
 #include "lcd.h"
 
+#include "juceRmlUi/rmlElemCanvas.h"
+#include "juceRmlUi/rmlEventListener.h"
+
 // LCD simulation
 
 namespace
@@ -28,162 +31,177 @@ namespace
 
 namespace jucePluginEditorLib
 {
-Lcd::Lcd(juce::Component& _parent, const uint32_t _width, const uint32_t _height)
-	: Button("LCDButton"),
-	m_parent(_parent),
-    m_scaleW(static_cast<float>(_parent.getWidth()) / (static_cast<float>(_width) * g_charSizeW + g_charSpacingW * (static_cast<float>(_width) - 1))),
-    m_scaleH(static_cast<float>(_parent.getHeight()) / (static_cast<float>(_height) * g_charSizeH + g_charSpacingH * (static_cast<float>(_height) - 1))),
-	m_width(_width),
-	m_height(_height)
-{
-	setSize(_parent.getWidth(), _parent.getHeight());
-
+	Lcd::Lcd(Rml::Element* _parent, uint32_t _numCharsX, uint32_t _numCharsY)
+	: m_numCharsX(_numCharsX)
+	, m_numCharsY(_numCharsY)
 	{
-		const std::string bgColor = _parent.getProperties().getWithDefault("lcdBackgroundColor", juce::String()).toString().toStdString();
-		if (bgColor.size() == 6)
-			m_charBgColor = strtol(bgColor.c_str(), nullptr, 16) | 0xff000000;
-	}
+		m_canvas = juceRmlUi::ElemCanvas::create(_parent);
 
-	{
-		const std::string color = _parent.getProperties().getWithDefault("lcdTextColor", juce::String()).toString().toStdString();
-		if (color.size() == 6)
-			m_charColor = strtol(color.c_str(), nullptr, 16) | 0xff000000;
-	}
+		m_canvas->setClearEveryFrame(true);
 
-	m_text.resize(_width * _height, 255);	// block character
-	m_overrideText.resize(_width * _height, 0);
-
-	m_cgData.fill({0});
-
-	onClick = [&]()
-	{
-		onClicked();
-	};
-
-	setEnabled(true);
-}
-
-Lcd::~Lcd() = default;
-
-void Lcd::setText(const std::vector<uint8_t>& _text)
-{
-	if (m_text == _text)
-		return;
-
-	m_text = _text;
-
-	repaint();
-}
-
-void Lcd::setCgRam(const std::array<uint8_t, 64>& _data)
-{
-	for (uint8_t i=0; i<static_cast<uint8_t>(m_cgData.size()); ++i)
-	{
-		std::array<uint8_t, 8> c{};
-		memcpy(c.data(), &_data[i*8], 8);
-
-		if (c != m_cgData[i])
+		m_canvas->setRepaintGraphicsCallback([this](const juce::Image& _image, juce::Graphics& _graphics)
 		{
-			m_cgData[i] = c;
-			m_characterPaths[i] = createPath(i);
+			paint(_image, _graphics);
+		});
+
+		{
+			const std::string bgColor = juceRmlUi::Element::getProperty(_parent, "lcdBackgroundColor", std::string());
+			if (bgColor.size() == 6)
+				m_charBgColor = strtol(bgColor.c_str(), nullptr, 16) | 0xff000000;
 		}
+
+		{
+			const std::string color = juceRmlUi::Element::getProperty(_parent, "lcdTextColor", std::string());
+			if (color.size() == 6)
+				m_charColor = strtol(color.c_str(), nullptr, 16) | 0xff000000;
+		}
+
+		m_text.resize(_numCharsX * _numCharsY, 255);	// block character
+		m_overrideText.resize(_numCharsX * _numCharsY, 0);
+
+		m_cgData.fill({});
+
+		juceRmlUi::EventListener::AddClick(m_canvas, [this]
+		{
+			onClicked();
+		});
 	}
 
-	repaint();
-}
+	Lcd::~Lcd() = default;
 
-void Lcd::postConstruct()
-{
-	for (uint32_t i=16; i<256; ++i)
-		m_characterPaths[i] = createPath(static_cast<uint8_t>(i));
-
-	m_parent.addAndMakeVisible(this);
-}
-
-void Lcd::paint(juce::Graphics& _g)
-{
-	const auto& text = m_overrideText[0] ? m_overrideText : m_text;
-
-	uint32_t charIdx = 0;
-
-	for (uint32_t y=0; y < m_height; ++y)
+	void Lcd::setText(const std::vector<uint8_t>& _text)
 	{
-		const auto ty = static_cast<float>(y) * g_charStrideH * m_scaleH;
+		if (m_text == _text)
+			return;
 
-		for (uint32_t x = 0; x < m_width; ++x, ++charIdx)
+		m_text = _text;
+
+		repaint();
+	}
+
+	void Lcd::setCgRam(const std::array<uint8_t, 64>& _data)
+	{
+		for (uint8_t i=0; i<static_cast<uint8_t>(m_cgData.size()); ++i)
 		{
-			const auto tx = static_cast<float>(x) * g_charStrideW * m_scaleW;
+			std::array<uint8_t, 8> c{};
+			memcpy(c.data(), &_data[i*8], 8);
 
-			const auto t = juce::AffineTransform::translation(tx, ty);
-
-			const auto c = text[charIdx];
-			const auto& p = m_characterPaths[c];
-
-			if (m_charBgColor)
+			if (c != m_cgData[i])
 			{
-				_g.setColour(juce::Colour(m_charBgColor));
-				_g.fillPath(m_characterPaths[255], t);
+				m_cgData[i] = c;
+				m_characterPaths[i] = createPath(i);
 			}
-			_g.setColour(juce::Colour(m_charColor));
-			_g.fillPath(p, t);
 		}
+
+		repaint();
 	}
-}
 
-juce::Path Lcd::createPath(const uint8_t _character) const
-{
-	const auto* data = _character < m_cgData.size() ? m_cgData[_character].data() : getCharacterData(_character);
-
-	juce::Path path;
-
-	const auto h = g_pixelSizeH * m_scaleH;
-	const auto w = g_pixelSizeW * m_scaleW;
-
-	for (auto y=0; y<8; ++y)
+	void Lcd::setSize(const uint32_t _width, const uint32_t _height)
 	{
-		const auto y0 = static_cast<float>(y) * g_pixelStrideH * m_scaleH;
+		if (m_width == _width && m_height == _height)
+			return;
 
-		for (auto x=0; x<=4; ++x)
+		m_width = _width;
+		m_height = _height;
+
+		m_scaleW = static_cast<float>(m_width) / (static_cast<float>(m_numCharsX) * g_charSizeW + g_charSpacingW * (static_cast<float>(m_numCharsX) - 1));
+	    m_scaleH = static_cast<float>(m_height) / (static_cast<float>(m_numCharsY) * g_charSizeH + g_charSpacingH * (static_cast<float>(m_numCharsY) - 1));
+
+		for (uint32_t i=0; i<m_characterPaths.size(); ++i)
+			m_characterPaths[i] = createPath(static_cast<uint8_t>(i));
+	}
+
+	void Lcd::paint(const juce::Image& _image, juce::Graphics& _g)
+	{
+		setSize(static_cast<uint32_t>(_image.getWidth()), static_cast<uint32_t>(_image.getHeight()));
+
+		const auto& text = m_overrideText[0] ? m_overrideText : m_text;
+
+		uint32_t charIdx = 0;
+
+		for (uint32_t y=0; y < m_numCharsY; ++y)
 		{
-			const auto bit = 4-x;
+			const auto ty = static_cast<float>(y) * g_charStrideH * m_scaleH;
 
-			const auto set = data[y] & (1<<bit);
+			for (uint32_t x = 0; x < m_numCharsX; ++x, ++charIdx)
+			{
+				const auto tx = static_cast<float>(x) * g_charStrideW * m_scaleW;
 
-			if(!set)
-				continue;
+				const auto t = juce::AffineTransform::translation(tx, ty);
 
-			const auto x0 = static_cast<float>(x) * g_pixelStrideW * m_scaleW;
+				const auto c = text[charIdx];
+				const auto& p = m_characterPaths[c];
 
-			path.addRectangle(x0, y0, w, h);
+				if (m_charBgColor)
+				{
+					_g.setColour(juce::Colour(m_charBgColor));
+					_g.fillPath(m_characterPaths[255], t);
+				}
+				_g.setColour(juce::Colour(m_charColor));
+				_g.fillPath(p, t);
+			}
 		}
 	}
 
-	return path;
-}
-
-void Lcd::onClicked()
-{
-	if(isTimerRunning())
-		return;
-
-	std::vector<std::vector<uint8_t>> lines;
-	getOverrideText(lines);
-
-	for (auto& c : m_overrideText)
-		c = ' ';
-
-	for(size_t y=0; y<std::min(lines.size(), static_cast<size_t>(m_height)); ++y)
+	juce::Path Lcd::createPath(const uint8_t _character) const
 	{
-		memcpy(&m_overrideText[m_width*y], lines[y].data(), std::min(lines[y].size(), static_cast<size_t>(m_width)));
+		const auto* data = _character < m_cgData.size() ? m_cgData[_character].data() : getCharacterData(_character);
+
+		juce::Path path;
+
+		const auto h = g_pixelSizeH * m_scaleH;
+		const auto w = g_pixelSizeW * m_scaleW;
+
+		for (auto y=0; y<8; ++y)
+		{
+			const auto y0 = static_cast<float>(y) * g_pixelStrideH * m_scaleH;
+
+			for (auto x=0; x<=4; ++x)
+			{
+				const auto bit = 4-x;
+
+				const auto set = data[y] & (1<<bit);
+
+				if(!set)
+					continue;
+
+				const auto x0 = static_cast<float>(x) * g_pixelStrideW * m_scaleW;
+
+				path.addRectangle(x0, y0, w, h);
+			}
+		}
+
+		return path;
 	}
 
-	startTimer(3000);
-}
+	void Lcd::onClicked()
+	{
+		if(isTimerRunning())
+			return;
 
-void Lcd::timerCallback()
-{
-	stopTimer();
-	m_overrideText[0] = 0;
-	repaint();
-}
+		std::vector<std::vector<uint8_t>> lines;
+		getOverrideText(lines);
+
+		for (auto& c : m_overrideText)
+			c = ' ';
+
+		for(size_t y=0; y<std::min(lines.size(), static_cast<size_t>(m_numCharsY)); ++y)
+		{
+			memcpy(&m_overrideText[m_numCharsX*y], lines[y].data(), std::min(lines[y].size(), static_cast<size_t>(m_numCharsX)));
+		}
+
+		startTimer(3000);
+	}
+
+	void Lcd::repaint() const
+	{
+		m_canvas->repaint();
+	}
+
+	void Lcd::timerCallback()
+	{
+		stopTimer();
+		m_overrideText[0] = 0;
+		repaint();
+	}
 }
