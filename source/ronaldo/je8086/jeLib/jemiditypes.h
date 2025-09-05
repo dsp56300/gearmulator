7484f2b5c2ecf60a4b73767b9c3fec53dd6ee4ff
dsp56300 <dsp56300@users.noreply.github.com>
2025-09-05 17:53:41 +0200

    define midi types WIP

--- source/ronaldo/je8086/jeLib/jemiditypes.h
+++ source/ronaldo/je8086/jeLib/jemiditypes.h
@@ -4,9 +4,11 @@ namespace jeLib
 {
 	enum AddressArea
 	{
-		System          = 0x00000000,
-		PerformanceTemp = 0x01000000,
-		UserPatch       = 0x02000000, // User Patch (Patch U:A11 - U:B88)
-		UserPerformance = 0x03000000, // User Performance (Performance U:11 - U:88)
+		System             = 0x00000000,
+		PerformanceTemp    = 0x01000000,
+		UserPatch          = 0x02000000, // User Patch (Patch U:A11 - U:B88)
+		UserPerformance    = 0x03000000, // User Performance (Performance U:11 - U:88)
+		MotionControlDataA = 0x09000000, // Motion Control Data (Motion Set A)
+		MotionControlDataB = 0x0A000000, // Motion Control Data (Motion Set B)
 	};
 }
