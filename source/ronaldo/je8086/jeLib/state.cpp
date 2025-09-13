da0adeff49e80a1ed6eeb56ab3a4097865ac71b3
dsp56300 <dsp56300@users.noreply.github.com>
2025-09-13 15:32:56 +0200

    lots of sysex helper functions, wip

--- source/ronaldo/je8086/jeLib/state.cpp
+++ source/ronaldo/je8086/jeLib/state.cpp
@@ -1,5 +1,7 @@
 #include "state.h"
 
+#include <cassert>
+
 #include "jemiditypes.h"
 
 namespace jeLib
@@ -104,5 +106,140 @@ namespace jeLib
 		}
 
 		return name;
+	}
+
+	bool State::is14BitData(const Patch _param)
+	{
+		switch (_param)
+		{
+		case Patch::MorphBendAssign:
+		case Patch::VelocitySwitch:
+			return false;
+		default:
+			return _param >= Patch::ControlLfo1Rate && _param <= Patch::VelocityPortamentoTime;
+		}
+	}
+
+	State::Dump State::createHeader(SysexByte _command, SysexByte _deviceId, const rLib::Storage::Address4& _address)
+	{
+		return {
+			static_cast<uint8_t>(SysexByte::SOX),
+			static_cast<uint8_t>(SysexByte::ManufacturerID),
+			static_cast<uint8_t>(_deviceId),
+			static_cast<uint8_t>(SysexByte::ModelIdMSB),
+			static_cast<uint8_t>(SysexByte::ModelIdLSB),
+			static_cast<uint8_t>(_command),
+			_address[0],
+			_address[1],
+			_address[2],
+			_address[3],
+			// Data follows
+		};
+	}
+
+	uint8_t State::calcChecksum(const Dump& _dump)
+	{
+		constexpr size_t start = std::size(g_sysexHeader) + 1/*command*/;
+
+		assert(_dump.size() > start);
+
+		auto end = _dump.size();
+
+		if (match(_dump.back(), SysexByte::EOX))
+			end -= 2;
+
+		assert(end > start);
+
+		uint8_t checksum = 0;
+		for (size_t i = start; i < end; ++i)
+			checksum += _dump[i];
+		checksum &= 0x7f;
+		checksum = (128 - checksum) & 0x7f;
+
+		return checksum;
+	}
+
+	State::Dump& State::createFooter(Dump& _dump)
+	{
+		assert(!_dump.empty());
+		assert(_dump.back() != 0xf7);
+
+		_dump.push_back(calcChecksum(_dump));
+		_dump.push_back(static_cast<uint8_t>(SysexByte::EOX));
+
+		return _dump;
+	}
+
+	rLib::Storage::Address4 State::toAddress(const uint32_t _addr)
+	{
+		assert((_addr & 0x80808080) == 0); // Only 7 bits per byte are valid
+
+		return {
+			static_cast<uint8_t>((_addr >> 24) & 0x7f),
+			static_cast<uint8_t>((_addr >> 16) & 0x7f),
+			static_cast<uint8_t>((_addr >> 8) & 0x7f),
+			static_cast<uint8_t>(_addr & 0x7f),
+		};
+	}
+
+	UserPatchArea State::userPatchArea(uint32_t _index)
+	{
+		assert(_index < 128);
+
+		constexpr auto step = static_cast<uint32_t>(UserPatchArea::UserPatch002) - static_cast<uint32_t>(UserPatchArea::UserPatch001);
+
+		if (_index < 64)
+		{
+			return static_cast<UserPatchArea>(_index * step + static_cast<uint32_t>(UserPatchArea::UserPatch001));
+		}
+
+		_index -= 64;
+
+		return static_cast<UserPatchArea>(_index * step + static_cast<uint32_t>(UserPatchArea::UserPatch065));
+	}
+
+	UserPerformanceArea State::userPerformanceArea(const uint32_t _index)
+	{
+		assert(_index < 64);
+		constexpr auto step = static_cast<uint32_t>(UserPerformanceArea::UserPerformance02) - static_cast<uint32_t>(UserPerformanceArea::UserPerformance01);
+		return static_cast<UserPerformanceArea>(_index * step + static_cast<uint32_t>(UserPerformanceArea::UserPerformance01));
+	}
+
+	rLib::Storage::Address4 State::toAddress(const PerformanceData _performanceData, Patch _param)
+	{
+		uint32_t addr = static_cast<uint32_t>(AddressArea::PerformanceTemp);
+		addr |= static_cast<uint32_t>(_performanceData);
+		addr |= static_cast<uint32_t>(_param);
+		return toAddress(addr);
+	}
+
+	State::Dump& State::addParameter(Dump& _dump, const Patch _param, int _paramValue)
+	{
+		if (is14BitData(_param))
+		{
+			_dump.push_back(static_cast<uint8_t>((_paramValue >> 7) & 0x7F));  // MSB
+			_dump.push_back(static_cast<uint8_t>(_paramValue & 0x7F));         // LSB
+		}
+		else
+		{
+			_dump.push_back(static_cast<uint8_t>(_paramValue & 0x7F));
+		}
+
+		return _dump;
+	}
+
+	State::Dump State::createParameterChange(const PerformanceData _performanceData, const Patch _param, const int _paramValue)
+	{
+		assert(
+			_performanceData == PerformanceData::PatchUpper ||
+			_performanceData == PerformanceData::PatchLower
+		);
+
+		const auto addr = toAddress(_performanceData, _param);
+		Dump dump = createHeader(SysexByte::CommandIdDataSet1, SysexByte::DeviceIdDefault, addr);
+
+		addParameter(dump, _param, _paramValue);
+
+		return createFooter(dump);
 	}
 }
--- source/ronaldo/je8086/jeLib/state.h
+++ source/ronaldo/je8086/jeLib/state.h
@@ -6,6 +6,7 @@
 #include <string>
 
 #include "jemiditypes.h"
+#include "common/storage.h"
 
 #include "jucePluginLib/patchdb/patchdbtypes.h"
 
@@ -26,6 +27,21 @@ namespace jeLib
 
 		static uint32_t getBankNumber(Address _addr);
 		static uint32_t getProgramNumber(Address _addr);
+
 		static std::optional<std::string> getName(const Dump& _dump);
+
+		static bool is14BitData(Patch _param);
+
+		static Dump createHeader(SysexByte _command, SysexByte _deviceId, const rLib::Storage::Address4& _address);
+		static uint8_t calcChecksum(const Dump& _dump);
+		static Dump& createFooter(Dump& _dump);
+
+		static rLib::Storage::Address4 toAddress(uint32_t _addr);
+		static UserPatchArea userPatchArea(uint32_t _index);
+		static UserPerformanceArea userPerformanceArea(uint32_t _index);
+		static rLib::Storage::Address4 toAddress(PerformanceData _performanceData, Patch _param);
+
+		static Dump& addParameter(Dump& _dump, Patch _param, int _paramValue);
+		static Dump createParameterChange(PerformanceData _performanceData, Patch _param, int _paramValue);
 	};
 }
