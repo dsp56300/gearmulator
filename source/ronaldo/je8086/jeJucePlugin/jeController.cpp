eb2aa5311ffadbe7025f8220c871796e4e6894e8
dsp56300 <dsp56300@users.noreply.github.com>
2025-09-13 17:13:59 +0200

    send parameter changes to device via sysex

--- source/ronaldo/je8086/jeJucePlugin/jeController.cpp
+++ source/ronaldo/je8086/jeJucePlugin/jeController.cpp
@@ -2,6 +2,10 @@
 
 #include "jePluginProcessor.h"
 
+#include "dsp56kEmu/logging.h"
+
+#include "jeLib/state.h"
+
 namespace jeJucePlugin
 {
 	Controller::Controller(AudioPluginAudioProcessor& _p) : pluginLib::Controller(_p, "parameterDescriptions_je.json")
@@ -26,6 +30,22 @@ namespace jeJucePlugin
 
 	void Controller::sendParameterChange(const pluginLib::Parameter& _parameter, pluginLib::ParamValue _value)
 	{
+		const auto& desc = _parameter.getDescription();
+
+		if (desc.page < 2)
+		{
+			// patch parameter
+
+			const auto index = desc.index + (desc.page * 128);
+
+			const auto lowerUpper = getCurrentPart() > 0 ? jeLib::PerformanceData::PatchLower : jeLib::PerformanceData::PatchUpper;
+
+			const auto msg = jeLib::State::createParameterChange(lowerUpper, static_cast<jeLib::Patch>(index), _value);
+
+			LOG("Send paramchange: " << _parameter.getDescription().name << " (" << index << ") = " << _value);
+
+			sendSysEx(msg);
+		}
 	}
 
 	bool Controller::parseSysexMessage(const pluginLib::SysEx&, synthLib::MidiEventSource)
