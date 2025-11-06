1e7e90f0f32813c4e159b9bfc034f91a8595ce7f
dsp56300 <dsp56300@users.noreply.github.com>
2025-11-06 23:13:46 +0100

    add write support to config file

--- source/baseLib/configFile.h
+++ source/baseLib/configFile.h
@@ -7,7 +7,10 @@ namespace baseLib
 	class ConfigFile : public PropertyMap
 	{
 	public:
+		ConfigFile() = default;
 		explicit ConfigFile(const char* _filename);
 		explicit ConfigFile(const std::string& _filename);
+
+		bool writeToFile(const std::string& _filename) const;
 	};
 }
