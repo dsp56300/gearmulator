2f3de4ccee14d28b4e8808fe21569d63d036976e
dsp56300 <dsp56300@users.noreply.github.com>
2026-01-21 20:21:59 +0100

    reload skin when toggling software renderer

--- source/jucePluginEditorLib/settingsGui.cpp
+++ source/jucePluginEditorLib/settingsGui.cpp
@@ -1,11 +1,19 @@
 #include "settingsGui.h"
 
+#include "pluginProcessor.h"
+#include "pluginEditorState.h"
+
 namespace jucePluginEditorLib
 {
 	void SettingsGui::createUi(Rml::Element* _root)
 	{
-		createToggleButton(_root, "btForceSoftwareRendering", "forceSoftwareRenderer", [](bool _enable)
+		createToggleButton(_root, "btForceSoftwareRendering", "forceSoftwareRenderer", [this](bool)
 		{
+			juce::MessageManager::callAsync([this]
+			{
+				auto* editorState = m_processor.getEditorState();
+				editorState->loadSkin(editorState->getCurrentSkin());
+			});
 		});
 	}
 }
