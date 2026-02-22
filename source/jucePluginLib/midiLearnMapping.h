458a534029babebeded30f16a6e2dfc19724afe6
dsp56300 <dsp56300@users.noreply.github.com>
2026-02-22 17:44:23 +0100

    fix forward declared enums

--- source/jucePluginLib/midiLearnMapping.h
+++ source/jucePluginLib/midiLearnMapping.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <cstdint>
 
-namespace synthLib { struct SMidiEvent; enum MidiStatusByte; enum class MidiEventSource : uint8_t; }
+#include "synthLib/midiTypes.h"
 
 namespace pluginLib
 {
