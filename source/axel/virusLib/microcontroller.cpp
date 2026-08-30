2a0b72cbc520e899b96850e7c93313e34ef18984
ChasonDeshotel <me@chasondeshotel.com>
2026-08-30 08:22:24 -0500

    virusLib: Add missing switch cases
    The selective ControlCommand switch now explicitly defaults to normal processing for all non-bank/program commands.
    
    PAGE_6E, PAGE_6F, and PAGE_D are explicitly acknowledged as having no fields duplicated into the multi edit buffer.

--- source/axel/virusLib/microcontroller.cpp
+++ source/axel/virusLib/microcontroller.cpp
@@ -817,6 +817,8 @@ bool Microcontroller::sendSysex(const synthLib::SysexBuffer& _data, std::vector<
 							return multiProgramChange(value);
 						}
 						return true;
+					default:
+						break;
 					}
 				}
 				else
@@ -1322,6 +1324,10 @@ void Microcontroller::applyToMultiEditBuffer(const Page _page, const uint8_t _pa
 			const auto idx = MD_PART_MIDI_CHANNEL + ((_param-PART_MIDI_CHANNEL)*16) + _part;
 			m_multiEditBuffer[idx] = _value;
 		}
+		break;
+	case PAGE_6E:
+	case PAGE_6F:
+	case PAGE_D:
 		break;
 	}
 }
