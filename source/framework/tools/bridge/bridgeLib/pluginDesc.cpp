805aa91d7251c95ae9b1122ec6e24c84cb807370
dsp56300 <dsp56300@users.noreply.github.com>
2026-09-15 19:33:26 +0200

    bridgeLib: remove unused pluginDesc stubs
    Leftovers from 41767be56. Never listed in CMakeLists.txt and nothing
    includes them; PluginDesc lives in commands.h/.cpp.

--- source/framework/tools/bridge/bridgeLib/pluginDesc.cpp
+++
@@ -1,9 +0,0 @@
-#include "pluginDesc.h"
-
-baseLib::BinaryStream& bridgeLib::PluginDesc::write(baseLib::BinaryStream& _s)
-{
-}
-
-baseLib::BinaryStream& bridgeLib::PluginDesc::read(baseLib::BinaryStream& _s)
-{
-}
